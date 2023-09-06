3a58060fd4506d219f97ebd1e956426d210953c5
alfonsobries <alfonsobries@users.noreply.github.com>
2023-09-06 17:26:42 +0000

    style: resolve style guide violations

--- app/Http/Middleware/ValidateSignedWallet.php
+++ app/Http/Middleware/ValidateSignedWallet.php
@@ -15,7 +15,7 @@ class ValidateSignedWallet
     /**
      * Handle an incoming request.
      *
-     * @param \Closure(Request):Response $next
+     * @param  \Closure(Request):Response  $next
      */
     public function handle(Request $request, Closure $next): Response|JsonResponse
     {
