65690eb92286c875e5656939b5a66b719f147d4c
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-09-06 07:44:26 -0500

    fix: remove unrelated files

--- vite.config.ts
+++ vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     plugins: [
         buildTranslations("lang", "resources/js/I18n/Locales"),
         laravel({
-            input: ["resources/js/app.tsx", 'resources/css/filament.css'],
+            input: "resources/js/app.tsx",
             refresh: true,
         }),
         process.env.NODE_ENV !== "production"
