4fc736383186e288dc80b43e0e11e85266243639
ItsANameToo <itsanametoo@protonmail.com>
2023-09-06 12:30:26 +0200

    refactor: additional stats

--- app/Filament/Widgets/OverviewStats.php
+++ app/Filament/Widgets/OverviewStats.php
@@ -10,6 +10,7 @@ use App\Models\Wallet;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class OverviewStats extends StatsOverviewWidget
 {
@@ -23,6 +24,18 @@ class OverviewStats extends StatsOverviewWidget
             Stat::make('Number of NFTs', static fn () => Cache::remember('filament:widgets:nfts', $ttl, static fn () => number_format(Nft::count()))),
 
             Stat::make('Number of wallet-owned NFTs', static fn () => Cache::remember('filament:widgets:owned-nfts', $ttl, static fn () => number_format(Nft::whereNotNull('wallet_id')->count()))),
+
+            Stat::make('Number of collections', static fn () => Cache::remember('filament:widgets:collections', $ttl,
+                static function () {
+                    return Collection::count();
+                }
+            )),
+
+            Stat::make('Number of collections for wallet-owned NFTs', static fn () => Cache::remember('filament:widgets:collections-owned-nfts', $ttl,
+                static function () {
+                    return Nft::whereNotNull('wallet_id')->distinct('collection_id')->count();
+                }
+            )),
 
             Stat::make('Average NFTs per wallet', static fn () => Cache::remember('filament:widgets:nfts-per-wallet', $ttl,
                 static function () {
@@ -48,17 +61,22 @@ class OverviewStats extends StatsOverviewWidget
                 }
             )),
 
-            Stat::make('Number of collections for wallet-owned NFTs', static fn () => Cache::remember('filament:widgets:collections-owned-nfts', $ttl,
-                static function () {
-                    return Nft::whereNotNull('wallet_id')->distinct('collection_id')->count();
-                }
-            )),
-
             Stat::make('Most NFTs in a wallet', static fn () => Cache::remember('filament:widgets:most-nfts', $ttl,
                 static function () {
                     return Wallet::withCount('nfts')->latest('nfts_count')->first()->nfts_count;
                 }
             ))->description('Number of NFTs for a wallet that owns the most NFTs'),
+
+            Stat::make('Most unique collections owned by wallet', static fn () => Cache::remember('filament:widgets:most-collections', $ttl,
+                static function () {
+                    return DB::table('nfts')
+                        ->select(DB::raw("nfts.wallet_id, count(distinct(collection_id))"))
+                        ->groupBy('wallet_id')
+                        ->limit(1)
+                        ->first()
+                        ->count;
+                }
+            ))->description('Number of collections for a wallet that owns NFTs in the most collections'),
         ];
     }
 }
