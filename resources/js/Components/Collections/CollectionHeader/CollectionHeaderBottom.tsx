2f5c3c8a1f2a02dd50e689a4f1fd0ab5c19be57c
ItsANameToo <itsanametoo@protonmail.com>
2023-09-07 17:17:03 +0200

    wip

--- resources/js/Components/Collections/CollectionHeader/CollectionHeaderBottom.tsx
+++ resources/js/Components/Collections/CollectionHeader/CollectionHeaderBottom.tsx
@@ -23,74 +23,77 @@ export const CollectionHeaderBottom = ({ collection }: CollectionHeaderBottomPro
 
     return (
         <>
-            {user === null && <></>}
-            {user !== null && (
-                <div
-                    data-testid="CollectionHeaderBottom"
-                    className="hide-scrollbar flex items-center justify-between space-x-2 overflow-x-auto px-6 md-lg:mx-auto lg:mx-0 lg:space-x-6 lg:bg-theme-secondary-100 lg:px-6 lg:py-4"
-                >
-                    <GridHeader
-                        data-testid="CollectionHeaderBottom__floor"
-                        className="lg:border-r lg:border-theme-secondary-300 lg:pl-0 lg:pr-6"
-                        title={t("common.floor")}
-                        value={
-                            <FormatCrypto
-                                value={collection.floorPrice ?? "0"}
-                                token={token}
-                            />
-                        }
-                    />
-
-                    <GridHeader
-                        data-testid="CollectionHeaderBottom__volume"
-                        className="lg:border-r lg:border-theme-secondary-300 lg:pl-0 lg:pr-6"
-                        title={t("common.volume", { frequency: "" })}
-                        value={
-                            <FormatCrypto
-                                value={collection.volume ?? "0"}
-                                token={token}
-                            />
-                        }
-                    />
-
-                    <GridHeader
-                        data-testid="CollectionHeaderBottom__supply"
-                        className="lg:border-r lg:border-theme-secondary-300 lg:pl-0 lg:pr-6"
-                        title={t("common.supply")}
-                        value={isTruthy(collection.supply) ? <FormatNumber value={collection.supply} /> : null}
-                    />
-
-                    <GridHeader
-                        data-testid="CollectionHeaderBottom__owners"
-                        className="lg:border-r lg:border-theme-secondary-300 lg:pl-0 lg:pr-6"
-                        title={t("common.owners")}
-                        value={isTruthy(collection.owners) ? <FormatNumber value={collection.owners} /> : null}
-                    />
-
-                    <GridHeader
-                        data-testid="CollectionHeaderBottom__created"
-                        className="lg:px-0"
-                        title={t("common.created")}
-                        value={collection.mintedAt !== null ? toMonthYear(collection.mintedAt, user.attributes) : null}
-                    />
-
-                    <div className="flex lg:flex-1 lg:justify-end">
-                        <GridHeader
-                            data-testid="CollectionHeaderBottom__chain"
-                            className="w-full lg:w-auto lg:pr-0 lg:text-right"
-                            title={t("common.chain")}
-                            value={
-                                <NetworkIcon
-                                    className="lg:flex-row-reverse lg:space-x-0"
-                                    textClassName="lg:pr-2"
-                                    networkId={collection.chainId}
-                                    withoutTooltip
-                                />
-                            }
+            <div
+                data-testid="CollectionHeaderBottom"
+                className="hide-scrollbar flex items-center justify-between space-x-2 overflow-x-auto px-6 md-lg:mx-auto lg:mx-0 lg:space-x-6 lg:bg-theme-secondary-100 lg:px-6 lg:py-4"
+            >
+                <GridHeader
+                    data-testid="CollectionHeaderBottom__floor"
+                    className="lg:border-r lg:border-theme-secondary-300 lg:pl-0 lg:pr-6"
+                    title={t("common.floor")}
+                    value={
+                        <FormatCrypto
+                            value={collection.floorPrice ?? "0"}
+                            token={token}
                         />
-                    </div>
+                    }
+                />
+
+                <GridHeader
+                    data-testid="CollectionHeaderBottom__volume"
+                    className="lg:border-r lg:border-theme-secondary-300 lg:pl-0 lg:pr-6"
+                    title={t("common.volume", { frequency: "" })}
+                    value={
+                        <FormatCrypto
+                            value={collection.volume ?? "0"}
+                            token={token}
+                        />
+                    }
+                />
+
+                <GridHeader
+                    data-testid="CollectionHeaderBottom__supply"
+                    className="lg:border-r lg:border-theme-secondary-300 lg:pl-0 lg:pr-6"
+                    title={t("common.supply")}
+                    value={isTruthy(collection.supply) ? <FormatNumber value={collection.supply} /> : null}
+                />
+
+                <GridHeader
+                    data-testid="CollectionHeaderBottom__owners"
+                    className="lg:border-r lg:border-theme-secondary-300 lg:pl-0 lg:pr-6"
+                    title={t("common.owners")}
+                    value={isTruthy(collection.owners) ? <FormatNumber value={collection.owners} /> : null}
+                />
+
+                <GridHeader
+                    data-testid="CollectionHeaderBottom__created"
+                    className="lg:px-0"
+                    title={t("common.created")}
+                    value={
+                        // User can be null when useEffect has not been triggered yet
+                        // to set the ActiveUserContext, see https://app.clickup.com/t/861naue4m
+                        collection.mintedAt !== null && user !== null
+                            ? toMonthYear(collection.mintedAt, user!.attributes)
+                            : null
+                    }
+                />
+
+                <div className="flex lg:flex-1 lg:justify-end">
+                    <GridHeader
+                        data-testid="CollectionHeaderBottom__chain"
+                        className="w-full lg:w-auto lg:pr-0 lg:text-right"
+                        title={t("common.chain")}
+                        value={
+                            <NetworkIcon
+                                className="lg:flex-row-reverse lg:space-x-0"
+                                textClassName="lg:pr-2"
+                                networkId={collection.chainId}
+                                withoutTooltip
+                            />
+                        }
+                    />
                 </div>
-            )}
+            </div>
         </>
     );
 };
