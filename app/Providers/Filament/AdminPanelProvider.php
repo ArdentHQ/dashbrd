db1c5636430361d25448b78a01669e6be434b645
goga-m <goga.mamoulasvili@gmail.com>
2023-09-11 12:02:29 +0200

    wip

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -142,7 +142,7 @@ class AdminPanelProvider extends PanelProvider
                         slug: 'my-profile',
                     )
                     ->enableTwoFactorAuthentication(
-                        force: false,
+                        force: config("filament.enabled"),
                     ),
             ]);
     }
