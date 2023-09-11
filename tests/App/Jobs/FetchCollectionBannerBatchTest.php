2cbc57802c993bf2171218ed200a7552320e06cc
shahin-hq <shahin-hq@users.noreply.github.com>
2023-09-11 13:45:08 +0000

    style: resolve style guide violations

--- tests/App/Jobs/FetchCollectionBannerBatchTest.php
+++ tests/App/Jobs/FetchCollectionBannerBatchTest.php
@@ -20,7 +20,7 @@ it('should fetch nft collection banner', function () {
         'address' => '0xe785e82358879f061bc3dcac6f0444462d4b5330',
         'network_id' => $network->id,
         'extra_attributes' => [
-            'banner' => null
+            'banner' => null,
         ],
     ]);
 
@@ -32,7 +32,6 @@ it('should fetch nft collection banner', function () {
 
     expect($collection->banner())->toBe('https://i.seadn.io/gae/GHhptRLebBOWOy8kfXpYCVqsqdes-1-6I_jbuRnGTHHW6TD63CtciH75Dotfu2u8v6EmkWt-tjhkFRVLxRUwgMfKqqy5W24AolJayeo?w=500&auto=format');
 });
-
 
 it('should use the collection addresses and network id as a unique job identifier', function () {
     $network = Network::polygon()->firstOrFail();
