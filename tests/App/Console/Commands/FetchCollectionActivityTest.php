d004a811f9b1f9b56690d6c32d2716a8a4ccf086
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2023-09-11 13:14:00 +0200

    add cli test

---
+++ tests/App/Console/Commands/FetchCollectionActivityTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Jobs\FetchCollectionActivity;
+use App\Models\Collection;
+use Illuminate\Support\Facades\Bus;
+
+it('dispatches a job only for collections that are not currently already retrieving their activity', function () {
+    Bus::fake();
+
+    Collection::factory()->create([
+        'is_fetching_activity' => true,
+    ]);
+
+    Collection::factory()->create([
+        'is_fetching_activity' => false,
+    ]);
+
+    Bus::assertDispatchedTimes(FetchCollectionActivity::class, 0);
+
+    $this->artisan('nfts:fetch-collection-activity');
+
+    Bus::assertDispatchedTimes(FetchCollectionActivity::class, 1);
+});
+
+it('dispatches a job for a specific collection', function () {
+    Bus::fake();
+
+    $collection = Collection::factory()->create();
+
+    Bus::assertDispatchedTimes(FetchCollectionActivity::class, 0);
+
+    $this->artisan('nfts:fetch-collection-activity', [
+        '--collection-id' => $collection->id,
+    ]);
+
+    Bus::assertDispatchedTimes(FetchCollectionActivity::class, 1);
+});
