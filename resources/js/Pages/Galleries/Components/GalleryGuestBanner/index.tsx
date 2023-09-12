eaf4b1bd1232397fbe546800912e9157f3370107
goga-m <goga.mamoulasvili@gmail.com>
2023-09-12 11:30:46 +0200

    wip

--- resources/js/Pages/Galleries/Components/GalleryGuestBanner/index.tsx
+++ resources/js/Pages/Galleries/Components/GalleryGuestBanner/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Button } from "@/Components/Buttons";
-import { Icon } from "@/Components/Icon";
 import { useBreakpoint } from "@/Hooks/useBreakpoint";
 
 import { type MetaMaskState } from "@/Hooks/useMetaMask";
