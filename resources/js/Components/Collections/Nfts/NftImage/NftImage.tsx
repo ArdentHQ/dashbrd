f66bf41f243798106241058b8692a917ad96634c
patricio0312rev <patricio0312rev@users.noreply.github.com>
2023-09-18 13:22:46 +0000

    style: resolve style guide violations

--- resources/js/Components/Collections/Nfts/NftImage/NftImage.tsx
+++ resources/js/Components/Collections/Nfts/NftImage/NftImage.tsx
@@ -16,7 +16,6 @@ export const NftImage = ({ nft }: Properties): JSX.Element => {
         window.open(originalNftImage as string, "_blank");
     };
 
-
     return (
         <>
             <GalleryCard
