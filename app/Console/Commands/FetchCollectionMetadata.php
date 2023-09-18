d4c20deab58ca3753a4992bc52e455e978b63e3a
Shahin Safaraliyev <shahin@ardenthq.com>
2023-09-18 13:00:09 +0400

    refactor: combine collection metadata updates

---
+++ app/Console/Commands/FetchCollectionMetadata.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Jobs\FetchCollectionMetadataJob;
+use App\Models\Collection;
+use App\Models\Network;
+use Illuminate\Console\Command;
+use Illuminate\Support\Collection as IlluminateCollection;
+use Illuminate\Support\Facades\Log;
+
+class FetchCollectionMetadata extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'nfts:fetch-collection-metadata';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch collection metadata in batch';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        $networks = Network::query()->whereIn('id', function ($query) {
+            $query->select('network_id')
+                ->from('collections')
+                ->groupBy('network_id');
+        })->get();
+
+        $networks->map(function ($network) {
+            Collection::query()
+                ->select(['id', 'address'])
+                ->where('network_id', '=', $network->id)
+                ->chunkById(100, function (IlluminateCollection $collections) use ($network) {
+                    $addresses = $collections->pluck('address')->toArray();
+
+                    Log::info('Dispatching FetchCollectionBannerBatchJob', [
+                        'network_id' => $network->id,
+                        'collection_addresses' => $addresses,
+                    ]);
+
+                    FetchCollectionMetadataJob::dispatch($addresses, $network);
+                });
+        });
+
+        return Command::SUCCESS;
+    }
+}
