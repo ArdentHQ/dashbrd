c6e43baccf6bee360fffea64849ec8cc37c72530
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2023-09-19 02:56:43 -0500

    refactor: nft details page (#90)

---
+++ resources/js/Components/Collections/Nfts/NftHeader/NftBasicInfo.test.tsx
@@ -0,0 +1,68 @@
+import { router } from "@inertiajs/react";
+import { NftBasicInfo } from "./NftBasicInfo";
+import NFTCollectionFactory from "@/Tests/Factories/Nfts/NFTCollectionFactory";
+import NftFactory from "@/Tests/Factories/Nfts/NftFactory";
+import { render, screen } from "@/Tests/testing-library";
+
+describe("NftBasicInfo", () => {
+    it("should render", () => {
+        const nft = new NftFactory().create({});
+
+        render(<NftBasicInfo nft={nft} />);
+
+        expect(screen.getByTestId("NftBasicInfo__container")).toBeInTheDocument();
+    });
+
+    it("should render collection image and name", () => {
+        const collection = new NFTCollectionFactory().create();
+
+        const nft = new NftFactory().create({
+            collection,
+        });
+
+        render(<NftBasicInfo nft={nft} />);
+
+        expect(screen.getByTestId("NftHeader__collectionImage")).toBeInTheDocument();
+        expect(screen.getByTestId("NftHeader__collectionName")).toBeInTheDocument();
+    });
+
+    it("should render url to collection page", () => {
+        const collection = new NFTCollectionFactory().create();
+
+        const nft = new NftFactory().create({
+            collection,
+        });
+
+        render(<NftBasicInfo nft={nft} />);
+
+        const function_ = vi.fn();
+        const routerSpy = vi.spyOn(router, "visit").mockImplementation(function_);
+
+        screen.getByTestId("NftHeader__collectionName").click();
+
+        expect(routerSpy).toBeCalledTimes(1);
+    });
+
+    it("should render nft name if available", () => {
+        const nft = new NftFactory().create({
+            name: "Nft name",
+        });
+
+        render(<NftBasicInfo nft={nft} />);
+
+        expect(screen.getByTestId("NftHeader__heading")).toBeInTheDocument();
+        expect(screen.getByTestId("NftHeader__heading")).toHaveTextContent("Nft name");
+    });
+
+    it("should render nft number if name is not available", () => {
+        const nft = new NftFactory().create({
+            name: null,
+            tokenNumber: "1",
+        });
+
+        render(<NftBasicInfo nft={nft} />);
+
+        expect(screen.getByTestId("NftHeader__heading")).toBeInTheDocument();
+        expect(screen.getByTestId("NftHeader__heading")).toHaveTextContent("1");
+    });
+});
--- resources/js/Components/Collections/Nfts/NftHeader/NftHeader.tsx
+++ resources/js/Components/Collections/Nfts/NftHeader/NftHeader.tsx
@@ -1,108 +1,82 @@
-import { router } from "@inertiajs/react";
-import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 import { CollectionDescription } from "@/Components/Collections/CollentionDescription";
-import { Heading } from "@/Components/Heading";
-import { Img } from "@/Components/Image";
-import { Link, LinkButton } from "@/Components/Link";
-import { NetworkIcon } from "@/Components/Networks/NetworkIcon";
+import { NftActions } from "@/Components/Collections/Nfts/NftHeader/NftActions";
+import { NftBasicInfo } from "@/Components/Collections/Nfts/NftHeader/NftBasicInfo";
+import { NftOwner } from "@/Components/Collections/Nfts/NftHeader/NftOwner";
 import { Point } from "@/Components/Point";
-import { useNetwork } from "@/Hooks/useNetwork";
-import { formatAddress } from "@/Utils/format-address";
-import { TruncateMiddle } from "@/Utils/TruncateMiddle";
+import { useBreakpoint } from "@/Hooks/useBreakpoint";
 
 interface Properties {
     nft: App.Data.Nfts.NftData;
+    alreadyReported?: boolean;
+    reportAvailableIn?: string | null;
+    reportReasons?: Record<string, string>;
 }
 
-export const NftHeader = ({ nft }: Properties): JSX.Element => {
+export const NftHeader = ({ nft, alreadyReported, reportAvailableIn, reportReasons }: Properties): JSX.Element => {
     const { t } = useTranslation();
-    const { isPolygon } = useNetwork();
-    const address = nft.wallet != null ? formatAddress(nft.wallet.address) : null;
+    const { isLgAndAbove } = useBreakpoint();
 
-    const ownerUrl = useMemo(() => {
-        if (nft.wallet === null) {
-            return null;
-        }
-
-        return t(`urls.explorers.${isPolygon(nft.collection.chainId) ? "polygonscan" : "etherscan"}.addresses`, {
-            address,
-        });
-    }, [nft, t]);
+    if (isLgAndAbove) {
+        return (
+            <div
+                className="flex w-full justify-between"
+                data-testId="NftHeader__desktop"
+            >
+                <div className="flex w-full flex-col gap-2">
+                    <NftBasicInfo nft={nft} />
+                    <div className="flex flex-col gap-2 xl:flex-row xl:items-center">
+                        <NftOwner nft={nft} />
+                        <div className="hidden xl:block">
+                            <Point />
+                        </div>
+                        <div className="flex flex-row items-center gap-2">
+                            <CollectionDescription
+                                name={t("pages.nfts.about_nft")}
+                                description={nft.collection.description}
+                                linkClassName="font-medium text-sm"
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-end">
+                    <NftActions
+                        alreadyReported={alreadyReported}
+                        className="justify-center"
+                        nft={nft}
+                        reportAvailableIn={reportAvailableIn}
+                        reportReasons={reportReasons}
+                    />
+                </div>
+            </div>
+        );
+    }
 
     return (
         <>
-            <div className="mb-0.5 flex items-center gap-2">
-                {nft.collection.image != null && (
-                    <Img
-                        data-testid="NftHeader__collectionImage"
-                        src={nft.collection.image}
-                        alt={t("pages.nfts.collection_image")}
-                        className="h-4 w-4 rounded-full"
-                    />
-                )}
+            <div
+                className="flex w-max flex-row items-center justify-center gap-2 border-b border-solid border-theme-secondary-300 bg-theme-secondary-50 px-8 pb-4 pt-4 backdrop-blur xs:w-full sm:border-none sm:pb-4"
+                data-testId="NftHeader__mobile"
+            >
+                <NftOwner nft={nft} />
 
-                <LinkButton
-                    data-testid="NftHeader__collectionName"
-                    variant="link"
-                    textColor="text-theme-hint-600"
-                    fontSize="text-sm"
-                    onClick={() => {
-                        router.visit(
-                            route("collections.view", {
-                                slug: nft.collection.slug,
-                            }),
-                        );
-                    }}
-                >
-                    {nft.collection.name}
-                </LinkButton>
+                <Point />
+
+                <CollectionDescription
+                    name={t("pages.nfts.about_nft")}
+                    description={nft.collection.description}
+                    linkClassName="font-medium text-sm"
+                />
             </div>
 
-            <Heading
-                data-testid="NftHeader__heading"
-                level={2}
-                className="text-center lg:text-left"
-            >
-                {nft.name ?? nft.tokenNumber}
-            </Heading>
-
-            <div className="mt-2 flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                    <p className="text-xs font-medium text-theme-secondary-700 md:text-sm">
-                        {t("pages.nfts.owned_by") + " "}
-
-                        {nft.wallet !== null && ownerUrl !== null && address !== null ? (
-                            <Link
-                                data-testid="NftHeader__walletAddress"
-                                href={ownerUrl}
-                                external
-                                showExternalIcon={false}
-                            >
-                                <span className="text-theme-hint-600">
-                                    <TruncateMiddle
-                                        length={8}
-                                        text={address}
-                                    />
-                                </span>
-                            </Link>
-                        ) : (
-                            <span>{t("common.na")}</span>
-                        )}
-                    </p>
-
-                    <Point />
-
-                    <CollectionDescription
-                        name={t("pages.nfts.about_nft")}
-                        description={nft.collection.description}
-                        linkClassName="font-medium text-xs md:text-sm"
-                    />
-                </div>
-
-                <NetworkIcon
-                    networkId={nft.collection.chainId}
-                    className="hidden h-7 w-7 rounded-full bg-theme-secondary-100 p-1 lg:block"
+            <div className="mb-6 flex w-full flex-col gap-4 border-b border-solid border-theme-secondary-300 bg-white pb-6 pt-4">
+                <NftBasicInfo nft={nft} />
+                <NftActions
+                    alreadyReported={alreadyReported}
+                    className="justify-center"
+                    nft={nft}
+                    reportAvailableIn={reportAvailableIn}
+                    reportReasons={reportReasons}
                 />
             </div>
         </>
