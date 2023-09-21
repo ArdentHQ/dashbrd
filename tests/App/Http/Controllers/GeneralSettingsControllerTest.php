108a969c7a1287e72ee1bbebfbe2c0294490edff
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-09-21 10:13:47 -0600

    test that user cant open settings page

--- tests/App/Http/Controllers/GeneralSettingsControllerTest.php
+++ tests/App/Http/Controllers/GeneralSettingsControllerTest.php
@@ -5,38 +5,48 @@ declare(strict_types=1);
 use App\Enums\DateFormat;
 use App\Support\Facades\Signature;
 
-it('can render the General Settings page', function () {
-    $user = createUser();
+describe('user is not signed', function () {
+    it('cant render the General Settings page', function () {
+        $user = createUser();
 
-    $this->actingAs($user)
-        ->get(route('settings.general'))
-        ->assertStatus(200);
-});
+        $this->actingAs($user)
+            ->get(route('settings.general'))
+            ->assertRedirect();
+    });
 
-it('cant update user general preferences if not signed', function () {
-    $user = createUser([
-        'extra_attributes' => [
-            'currency' => 'USD',
-            'date_format' => DateFormat::D,
-            'time_format' => '24',
-            'timezone' => 'UTC',
-        ],
-    ]);
+    it('cant update user general preferences if not signed', function () {
+        $user = createUser([
+            'extra_attributes' => [
+                'currency' => 'USD',
+                'date_format' => DateFormat::D,
+                'time_format' => '24',
+                'timezone' => 'UTC',
+            ],
+        ]);
 
-    $this->actingAs($user)
-        ->put(route('settings.general'), [
-            'currency' => 'EUR',
-            'date_format' => DateFormat::C->value,
-            'time_format' => '12',
-            'timezone' => 'Europe/Amsterdam',
-        ])
-        ->assertRedirect();
+        $this->actingAs($user)
+            ->put(route('settings.general'), [
+                'currency' => 'EUR',
+                'date_format' => DateFormat::C->value,
+                'time_format' => '12',
+                'timezone' => 'Europe/Amsterdam',
+            ])
+            ->assertRedirect();
+    });
 });
 
 describe('user is signed', function () {
     beforeEach(function () {
         Signature::shouldReceive('walletIsSigned')
             ->andReturn(true);
+    });
+
+    it('can render the General Settings page', function () {
+        $user = createUser();
+
+        $this->actingAs($user)
+            ->get(route('settings.general'))
+            ->assertStatus(200);
     });
 
     it('can update user general preferences', function () {
