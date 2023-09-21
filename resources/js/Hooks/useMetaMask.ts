a3ed0ba4b89c224a284efc94a726016d1d3c950c
ItsANameToo <itsanametoo@protonmail.com>
2023-09-21 23:38:40 +0200

    wip

--- resources/js/Hooks/useMetaMask.ts
+++ resources/js/Hooks/useMetaMask.ts
@@ -62,8 +62,7 @@ const networkConfigs: Record<number, object> = {
     },
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
-const hasMetaMask = (): boolean => window.ethereum?.isMetaMask ?? false
+const hasMetaMask = (): boolean => window.ethereum?.isMetaMask ?? false;
 
 const getEthereum = (): Ethereum => window.ethereum as Ethereum;
 
