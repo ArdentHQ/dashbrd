b29aad718f586b2f4e8ce54bdbaad0447398e540
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2023-09-22 08:36:33 -0500

    Update resources/js/Components/Pagination/SelectPageLimit.tsx
    Co-authored-by: ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>

--- resources/js/Components/Pagination/SelectPageLimit.tsx
+++ resources/js/Components/Pagination/SelectPageLimit.tsx
@@ -19,7 +19,7 @@ export const SelectPageLimit = ({
 
     return (
         <Listbox
-            className="w-full xs:w-[12rem] sm:flex sm:justify-between"
+            className="w-full xs:w-48 sm:flex sm:justify-between"
             optionsClassName="right-0 w-32 bottom-16 rounded-xl"
             onChange={(value) => {
                 const urlParameters = new URLSearchParams(window.location.href);
