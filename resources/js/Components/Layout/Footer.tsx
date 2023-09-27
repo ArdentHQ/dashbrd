50bcae45c786f061a53bb86b049601055cf2abd1
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-09-27 08:08:36 -0500

    refactor: new footer design for tablet and mobile views

--- resources/js/Components/Layout/Footer.tsx
+++ resources/js/Components/Layout/Footer.tsx
@@ -2,6 +2,7 @@ import cn from "classnames";
 import { useTranslation } from "react-i18next";
 import { Icon } from "@/Components/Icon";
 import { Link } from "@/Components/Link";
+import { Point } from "@/Components/Point";
 import { PoweredByIcons } from "@/Components/PoweredByIcons";
 import { appName } from "@/Utils/app";
 
@@ -9,14 +10,68 @@ interface FooterProperties {
     withActionToolbar?: boolean;
 }
 
-const FooterSeparator = ({ className }: { className?: string }): JSX.Element => (
-    <span
-        className={cn(
-            "mx-2 h-1.25 w-1.25 rounded-full border border-theme-secondary-300 bg-theme-secondary-300 sm:mx-4",
-            className,
-        )}
-    ></span>
-);
+const PoweredBy = (): JSX.Element => {
+    const { t } = useTranslation();
+
+    return (
+        <div className="flex flex-row items-center">
+            <span className="mr-2 hidden text-xs font-medium text-theme-secondary-700 sm:block md:text-sm">
+                {t("footer.powered_by")}
+            </span>
+            <PoweredByIcons />
+        </div>
+    );
+};
+
+const SocialIcons = (): JSX.Element => {
+    const { t } = useTranslation();
+
+    return (
+        <div className="flex flex-row ">
+            <a
+                href={t("urls.twitter").toString()}
+                className="button-icon group h-8 w-8 border-transparent bg-transparent p-1 text-theme-secondary-700 hover:bg-theme-secondary-300 hover:text-theme-secondary-700 active:bg-theme-secondary-400 active:text-theme-primary-900 md:flex"
+                target="_blank"
+                rel="noopener nofollow noreferrer"
+            >
+                <span className="sr-only">Twitter</span>
+                <Icon
+                    name="Twitter"
+                    aria-hidden="true"
+                    size="md"
+                />
+            </a>
+
+            <a
+                href={t("urls.github").toString()}
+                className="button-icon group h-8 w-8 border-transparent bg-transparent p-1 text-theme-secondary-700 hover:bg-theme-secondary-300 hover:text-theme-secondary-700 active:bg-theme-secondary-400 active:text-theme-primary-900 md:flex"
+                target="_blank"
+                rel="noopener nofollow noreferrer"
+            >
+                <span className="sr-only">GitHub</span>
+                <Icon
+                    name="GitHub"
+                    aria-hidden="true"
+                    size="md"
+                />
+            </a>
+
+            <a
+                href={t("urls.discord").toString()}
+                className="button-icon group h-8 w-8 border-transparent bg-transparent p-1 text-theme-secondary-700 hover:bg-theme-secondary-300 hover:text-theme-secondary-700 active:bg-theme-secondary-400 active:text-theme-primary-900 md:flex"
+                target="_blank"
+                rel="noopener nofollow noreferrer"
+            >
+                <span className="sr-only">Discord</span>
+                <Icon
+                    name="Discord"
+                    aria-hidden="true"
+                    size="md"
+                />
+            </a>
+        </div>
+    );
+};
 
 export const Footer = ({ withActionToolbar = false }: FooterProperties): JSX.Element => {
     const { t } = useTranslation();
@@ -28,138 +83,58 @@ export const Footer = ({ withActionToolbar = false }: FooterProperties): JSX.Ele
                 "pb-16": withActionToolbar,
             })}
         >
-            <div className="mx-auto w-full max-w-content items-end justify-between px-6 py-4 text-sm sm:px-8 md-lg:flex md-lg:items-center lg:px-8 2xl:px-0">
-                <div className="text-theme-secondary-700">
-                    {new Date().getFullYear()} ©{" "}
-                    <Link
-                        variant="link"
-                        href={t("urls.landing")}
-                        textColor="text-theme-primary-600"
-                    >
-                        {appName()}
-                    </Link>
-                    . {t("footer.all_rights_reserved")}.
-                </div>
+            <div className="flex w-full max-w-content flex-col lg:flex-row lg:justify-between lg:px-8 xl:mx-auto 2xl:px-0">
+                <div className="flex flex-col px-6 pb-3 pt-4 sm:flex-row sm:justify-between sm:px-8 lg:gap-3 lg:px-0">
+                    <div className="text-xs font-medium text-theme-secondary-700 md:text-sm md:font-normal">
+                        {new Date().getFullYear()} ©{" "}
+                        <Link
+                            variant="link"
+                            href={t("urls.landing")}
+                            textColor="text-theme-primary-600"
+                            className="text-xs md:text-sm"
+                        >
+                            {appName()}
+                        </Link>
+                        . {t("footer.all_rights_reserved")}.
+                    </div>
 
-                <div className="mt-2 flex-1 items-center justify-between sm:flex md:mt-0">
-                    <div className="flex items-center">
-                        <FooterSeparator className="hidden md-lg:block" />
+                    <div className="flex flex-row items-center gap-2 sm:gap-3">
+                        <div className="hidden lg:flex">
+                            <Point />
+                        </div>
+
                         <Link
                             variant="link"
                             href={t("urls.privacy_policy")}
                             useAnchorTag
+                            className="text-xs md:text-sm"
                         >
                             {t("footer.privacy_policy")}
                         </Link>
 
-                        <FooterSeparator />
+                        <Point />
 
                         <Link
                             variant="link"
                             href={t("urls.terms_of_service")}
                             useAnchorTag
+                            className="text-xs md:text-sm"
                         >
                             {t("footer.terms_of_service")}
                         </Link>
 
-                        <div className="hidden items-center sm:flex">
-                            <FooterSeparator />
-
-                            <span className="pr-2 text-sm font-medium leading-5.5 text-theme-secondary-700">
-                                {t("footer.powered_by")}
-                            </span>
-
-                            <PoweredByIcons />
+                        <div className="hidden flex-row items-center gap-3 lg:flex">
+                            <Point />
+                            <PoweredBy />
                         </div>
                     </div>
-
-                    <div className="mt-2 flex flex-col justify-between xs:h-5 xs:flex-row xs:items-center sm:mt-0 sm:justify-end lg:justify-start">
-                        <div className="flex sm:hidden">
-                            <span className="pr-2 text-sm font-medium leading-5.5 text-theme-secondary-700">
-                                {t("footer.powered_by")}
-                            </span>
-                            <PoweredByIcons />
-                        </div>
-                        <div className="mt-2 flex space-x-4 xs:mt-0 md:space-x-0">
-                            <a
-                                href={t("urls.twitter").toString()}
-                                className="transition-default text-theme-secondary-700 hover:text-theme-primary-700 md:hidden"
-                                target="_blank"
-                                rel="noopener nofollow noreferrer"
-                            >
-                                <span className="sr-only">Twitter</span>
-                                <Icon
-                                    name="Twitter"
-                                    aria-hidden="true"
-                                />
-                            </a>
-
-                            <a
-                                href={t("urls.twitter").toString()}
-                                className="button-icon group hidden border-transparent bg-transparent text-theme-secondary-700 hover:text-theme-primary-900 md:flex"
-                                target="_blank"
-                                rel="noopener nofollow noreferrer"
-                            >
-                                <span className="sr-only">Twitter</span>
-                                <Icon
-                                    name="Twitter"
-                                    aria-hidden="true"
-                                />
-                            </a>
-
-                            <a
-                                href={t("urls.github").toString()}
-                                className="transition-default text-theme-secondary-700 hover:text-theme-primary-700 md:hidden"
-                                target="_blank"
-                                rel="noopener nofollow noreferrer"
-                            >
-                                <span className="sr-only">GitHub</span>
-                                <Icon
-                                    name="GitHub"
-                                    aria-hidden="true"
-                                />
-                            </a>
-
-                            <a
-                                href={t("urls.github").toString()}
-                                className="button-icon group !ml-0 hidden border-transparent bg-transparent text-theme-secondary-700 hover:text-theme-primary-900 md:flex"
-                                target="_blank"
-                                rel="noopener nofollow noreferrer"
-                            >
-                                <span className="sr-only">GitHub</span>
-                                <Icon
-                                    name="GitHub"
-                                    aria-hidden="true"
-                                />
-                            </a>
-
-                            <a
-                                href={t("urls.discord").toString()}
-                                className="transition-default text-theme-secondary-700 hover:text-theme-primary-700 md:hidden"
-                                target="_blank"
-                                rel="noopener nofollow noreferrer"
-                            >
-                                <span className="sr-only">Discord</span>
-                                <Icon
-                                    name="Discord"
-                                    aria-hidden="true"
-                                />
-                            </a>
-
-                            <a
-                                href={t("urls.discord").toString()}
-                                className="button-icon group !ml-0 hidden border-transparent bg-transparent text-theme-secondary-700 hover:text-theme-primary-900 md:flex"
-                                target="_blank"
-                                rel="noopener nofollow noreferrer"
-                            >
-                                <span className="sr-only">Discord</span>
-                                <Icon
-                                    name="Discord"
-                                    aria-hidden="true"
-                                />
-                            </a>
-                        </div>
-                    </div>
+                </div>
+                <div className="hidden items-center lg:flex">
+                    <SocialIcons />
+                </div>
+                <div className="flex w-full flex-row justify-between bg-theme-secondary-200 px-6 py-2 sm:px-8 lg:hidden">
+                    <PoweredBy />
+                    <SocialIcons />
                 </div>
             </div>
         </footer>
