060b74226d43493ccaa8fb14fecda5897e724ef6
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-09-27 13:38:54 -0600

    Update Index.tsx

--- resources/js/Pages/Galleries/MyGalleries/Index.tsx
+++ resources/js/Pages/Galleries/MyGalleries/Index.tsx
@@ -8,7 +8,6 @@ import { Icon } from "@/Components/Icon";
 import { LayoutWrapper } from "@/Components/Layout/LayoutWrapper";
 import { Pagination } from "@/Components/Pagination";
 import { Tooltip } from "@/Components/Tooltip";
-import { useAuthorizedAction } from "@/Hooks/useAuthorizedAction";
 
 const Index = ({
     title,
@@ -25,8 +24,6 @@ const Index = ({
     const userGalleries = galleries.paginated;
 
     const createGalleryUrl = route("my-galleries.create");
-
-    const { signedAction } = useAuthorizedAction();
 
     return (
         <LayoutWrapper>
