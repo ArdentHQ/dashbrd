c9b8d05ff9a730162aa9b4db85fe759befbc3d66
ItsANameToo <itsanametoo@protonmail.com>
2023-09-27 11:57:40 +0200

    wip

--- resources/js/Hooks/useMetaMask.ts
+++ resources/js/Hooks/useMetaMask.ts
@@ -657,7 +657,7 @@ const useMetaMask = ({ initialAuth }: Properties): MetaMaskState => {
         connectWallet,
         connecting,
         signing,
-        signed: true,
+        signed,
         initialized,
         needsMetaMask,
         supportsMetaMask,
