b07cfa9db371110d4db8e1bc81f02a4142045948
Shahin Safaraliyev <shahin@ardenthq.com>
2023-09-28 17:12:21 +0400

    feat: add article overview page

--- resources/js/Pages/Collections/Components/Articles/ArticlesTab.tsx
+++ resources/js/Pages/Collections/Components/Articles/ArticlesTab.tsx
@@ -8,8 +8,8 @@ import { ArticlePagination } from "@/Pages/Collections/Components/Articles/Artic
 import { ArticlesGrid } from "@/Pages/Collections/Components/Articles/ArticlesGrid";
 import { ArticlesList } from "@/Pages/Collections/Components/Articles/ArticlesList";
 import { ArticleSortDropdown } from "@/Pages/Collections/Components/Articles/ArticleSortDropdown";
+import { SamplePageMeta } from "@/Tests/SampleData";
 import { getQueryParameters } from "@/Utils/get-query-parameters";
-import {SamplePageMeta} from "@/Tests/SampleData";
 
 export const ArticlesTab = (): JSX.Element => {
     const { t } = useTranslation();
@@ -24,7 +24,7 @@ export const ArticlesTab = (): JSX.Element => {
     );
 
     // replace this with articles count
-    const articlesCount = 5;
+    const articlesCount = Math.random() * 10;
 
     return (
         <div>
@@ -61,8 +61,9 @@ export const ArticlesTab = (): JSX.Element => {
                     }}
                 />
             </div>
-            {/* eslint-disable @typescript-eslint/no-unnecessary-condition */}
+
             {articlesCount > 0 && displayType === DisplayTypes.Grid && <ArticlesGrid />}
+
             {articlesCount > 0 && displayType === DisplayTypes.List && <ArticlesList />}
 
             {articlesCount === 0 && query === "" && (
@@ -70,16 +71,13 @@ export const ArticlesTab = (): JSX.Element => {
                     <EmptyBlock>{t("pages.collections.articles.no_articles")}</EmptyBlock>
                 </div>
             )}
-
             {articlesCount === 0 && query !== "" && (
                 <div className="mt-6">
                     <EmptyBlock>{t("pages.collections.articles.no_articles")}</EmptyBlock>
                 </div>
             )}
             <ArticlePagination
-                {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-                // @ts-ignore
-                pagination={SamplePageMeta as PaginationData<unknown>}
+                pagination={SamplePageMeta.paginated as PaginationData<unknown>}
                 onPageLimitChange={(limit: number) => {
                     setPageLimit(limit);
                 }}
