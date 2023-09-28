f435870288a07b593d8d216214c0e2aeebd211bb
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-09-28 10:35:14 -0600

    make alchemy the default floor price provider

--- config/dashbrd.php
+++ config/dashbrd.php
@@ -67,7 +67,7 @@ return [
         App\Jobs\FetchCollectionNfts::class => 'alchemy', // No other provider gives us `totalSupply` that we use in jobs...
         App\Jobs\FetchEnsDetails::class => 'moralis',
         App\Jobs\FetchNativeBalances::class => env('WEB3_PROVIDER_FETCH_NATIVE_BALANCES', 'moralis'),
-        App\Jobs\FetchCollectionFloorPrice::class => env('WEB3_PROVIDER_FETCH_NFT_COLLECTION_FLOOR_PRICE', 'mnemonic'),
+        App\Jobs\FetchCollectionFloorPrice::class => env('WEB3_PROVIDER_FETCH_NFT_COLLECTION_FLOOR_PRICE', 'alchemy'),
         App\Jobs\DetermineCollectionMintingDate::class => env('WEB3_PROVIDER_DETERMINE_COLLECTION_MINTING_DATE', 'alchemy'),
     ],
 
