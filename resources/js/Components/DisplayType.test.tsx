a65b1d85953cfddd5a66dec559e1e6706582a966
Shahin Safaraliyev <shahin@ardenthq.com>
2023-09-28 18:58:51 +0400

    unit tests

---
+++ resources/js/Components/DisplayType.test.tsx
@@ -0,0 +1,32 @@
+import { expect } from "vitest";
+import { DisplayType, DisplayTypes } from "@/Components/DisplayType";
+import { render, screen, userEvent } from "@/Tests/testing-library";
+
+describe("DisplayType", () => {
+    it("should render the component", () => {
+        const { rerender } = render(<DisplayType displayType={DisplayTypes.List} />);
+
+        expect(screen.getByTestId("icon-Bars").closest("button")).toHaveClass("bg-white");
+        expect(screen.getByTestId("icon-GridDots").closest("button")).not.toHaveClass("bg-white");
+
+        rerender(<DisplayType displayType={DisplayTypes.Grid} />);
+
+        expect(screen.getByTestId("icon-Bars").closest("button")).not.toHaveClass("bg-white");
+        expect(screen.getByTestId("icon-GridDots").closest("button")).toHaveClass("bg-white");
+    });
+
+    it("should toggle display type when clicked", async () => {
+        const onSelect = vi.fn();
+
+        render(
+            <DisplayType
+                displayType={DisplayTypes.List}
+                onSelectDisplayType={onSelect}
+            />,
+        );
+
+        await userEvent.click(screen.getByTestId("icon-GridDots").closest("button") as HTMLButtonElement);
+
+        expect(onSelect).toHaveBeenCalledOnce();
+    });
+});
