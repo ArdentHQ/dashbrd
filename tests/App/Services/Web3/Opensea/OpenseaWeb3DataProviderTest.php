208c10b20a193b78378e308493b4f8a77dfba21d
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-09-28 20:04:25 -0600

    Update OpenseaWeb3DataProviderTest.php

--- tests/App/Services/Web3/Opensea/OpenseaWeb3DataProviderTest.php
+++ tests/App/Services/Web3/Opensea/OpenseaWeb3DataProviderTest.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Data\Web3\Web3NftCollectionFloorPrice;
+use App\Enums\Chains;
 use App\Exceptions\NotImplementedException;
 use App\Jobs\Middleware\RateLimited;
 use App\Models\Collection;
@@ -10,18 +12,39 @@ use App\Models\Wallet;
 use App\Services\Web3\Opensea\OpenseaWeb3DataProvider;
 use App\Support\Facades\Opensea;
 
-it('should getNftCollectionFloorPrice', function () {
-    Opensea::fake([
-        'https://api.footprint.network/api/v2/*' => Opensea::response(fixtureData('footprint.collection_metrics_no_supported')),
+it('should getNftCollectionFloorPrice if no open sea slug', function () {
+    Collection::factory()->create([
+        'address' => '0x23581767a106ae21c074b2276D25e5C3e136a68b',
+        'network_id' => Network::where('chain_id', Chains::ETH->value)->first()->id,
     ]);
 
-    $collectionSlug = 'doodles-official';
+    $contractAddress = '0x23581767a106ae21c074b2276D25e5C3e136a68b';
 
     $provider = new OpenseaWeb3DataProvider();
 
-    $data = $provider->getNftCollectionFloorPrice($collectionSlug);
+    $data = $provider->getNftCollectionFloorPrice(Chains::ETH, $contractAddress);
 
     expect($data)->toBeNull();
+});
+
+it('should getNftCollectionFloorPrice  ', function () {
+    Collection::factory()->create([
+        'address' => '0x23581767a106ae21c074b2276D25e5C3e136a68b',
+        'network_id' => Network::where('chain_id', Chains::ETH->value)->first()->id,
+        'extra_attributes' => ['opensea_slug' => 'testy'],
+    ]);
+
+    Opensea::fake([
+        'https://api.opensea.io/api/v1/collection*' => Opensea::response(fixtureData('opensea.collection_stats')),
+    ]);
+
+    $contractAddress = '0x23581767a106ae21c074b2276D25e5C3e136a68b';
+
+    $provider = new OpenseaWeb3DataProvider();
+
+    $data = $provider->getNftCollectionFloorPrice(Chains::ETH, $contractAddress);
+
+    expect($data)->toBeInstanceOf(Web3NftCollectionFloorPrice::class);
 });
 
 it('should getWalletTokens and throw NotImplementedException', function () {
