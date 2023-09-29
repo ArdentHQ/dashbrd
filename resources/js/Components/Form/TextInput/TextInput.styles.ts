39e0132eba00f08ff27f26f7bca042696a3db2c1
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-09-29 10:07:33 -0500

    feat: dark mode for text input

--- resources/js/Components/Form/TextInput/TextInput.styles.ts
+++ resources/js/Components/Form/TextInput/TextInput.styles.ts
@@ -15,20 +15,24 @@ export const textInputDynamicClassnames = ({
         classes.push("border-theme-danger-400 ring-1 ring-theme-danger-400");
     } else {
         if (isFocused) {
-            classes.push("border-theme-primary-600 ring-1 ring-theme-primary-600");
+            classes.push(
+                "border-theme-primary-600 ring-1 ring-theme-primary-600 dark:border-theme-primary-400 dark:ring-theme-primary-400",
+            );
         } else {
-            classes.push("border-theme-secondary-400");
+            classes.push("border-theme-secondary-400 dark:border-theme-dark-500");
 
             if (isMouseOver && !isDisabled) {
-                classes.push("ring-2 ring-theme-primary-100");
+                classes.push("ring-2 ring-theme-primary-100 dark:ring-theme-dark-700");
             }
         }
     }
 
     if (isDisabled) {
-        classes.push("bg-theme-secondary-50");
+        classes.push(
+            "bg-theme-secondary-50 dark:border-theme-dark-500 dark:bg-theme-dark-800 dark:text-theme-dark-200",
+        );
     } else {
-        classes.push("bg-white");
+        classes.push("bg-white dark:bg-theme-dark-900");
     }
 
     return classes;
