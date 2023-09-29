d40e9a91102e28bc25dcb5b0a2bf934d6fb8df16
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-09-29 09:32:38 -0500

    feat: dark mode for empty blocks

--- resources/js/Components/EmptyBlock/EmptyBlock.tsx
+++ resources/js/Components/EmptyBlock/EmptyBlock.tsx
@@ -14,7 +14,7 @@ export const EmptyBlock = ({
     <div
         data-testid="EmptyBlock"
         className={cn(
-            "rounded-xl border border-theme-secondary-300 p-4 text-center font-medium text-theme-secondary-700",
+            "rounded-xl border border-theme-secondary-300 p-4 text-center font-medium text-theme-secondary-700 dark:border-theme-dark-700 dark:text-theme-dark-200",
             textClasses,
             className,
         )}
