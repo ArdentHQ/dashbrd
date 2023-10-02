6059e431b9503313ab14249e2aa1bdd5d3cf727a
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-10-02 11:09:46 -0500

    fix: move refresh button to other branch

--- resources/js/Pages/Collections/Components/CollectionsHeading/CollectionsHeading.tsx
+++ resources/js/Pages/Collections/Components/CollectionsHeading/CollectionsHeading.tsx
@@ -1,7 +1,5 @@
 import { Trans, useTranslation } from "react-i18next";
-import { IconButton } from "@/Components/Buttons";
 import { Heading } from "@/Components/Heading";
-import { Tooltip } from "@/Components/Tooltip";
 import { formatFiat } from "@/Utils/Currency";
 import { tp } from "@/Utils/TranslatePlural";
 
@@ -21,26 +19,6 @@ export const CollectionsHeading = ({
         >
             <div className="flex w-full flex-row items-center justify-between gap-3 md:justify-start">
                 <h1 className="dark:text-theme-dark-50">{t("common.my_collection")}</h1>
-
-                <Tooltip
-                    content={
-                        <div className="flex flex-col">
-                            <span>{t("common.refresh_collection_title")}</span>
-                            <span className="text-xs text-theme-secondary-500">
-                                {t("common.refresh_collection_description")}
-                            </span>
-                        </div>
-                    }
-                    hideOnClick={false}
-                    zIndex={50}
-                >
-                    <IconButton
-                        data-testid={"Collection__refresh"}
-                        icon="Refresh"
-                        disabled={false}
-                        className="border-none bg-transparent md:border-solid"
-                    />
-                </Tooltip>
             </div>
             <span className="mt-1 block text-sm font-medium leading-5 text-theme-secondary-700 dark:text-theme-dark-200 sm:text-base">
                 <Trans
