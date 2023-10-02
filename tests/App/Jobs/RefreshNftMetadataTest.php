1a266b646d9aebfa0e7840c3191442f2e2032dae
goga-m <goga.mamoulasvili@gmail.com>
2023-10-02 14:51:52 +0200

    wip

--- tests/App/Jobs/RefreshNftMetadataTest.php
+++ tests/App/Jobs/RefreshNftMetadataTest.php
@@ -44,7 +44,7 @@ it('should refresh NFT metadata', function () {
 
     (new RefreshNftMetadata($collection, $nft))->handle(app(AlchemyWeb3DataProvider::class));
 
-    expect($collection->fresh()['name'])->toEqual('Moonbirds');
+    expect($collection->fresh()['name'])->toEqual('Unrevealed Collection');
     expect($nft->fresh()['name'])->toEqual('#0');
 });
 
