079a5a7144957196629f71b340ac3409a90d46d4
Shahin Safaraliyev <shahin@ardenthq.com>
2023-10-02 20:27:23 +0400

    wip

--- resources/js/Components/Articles/ArticleCard/ArticleCard.test.tsx
+++ resources/js/Components/Articles/ArticleCard/ArticleCard.test.tsx
@@ -2,33 +2,26 @@ import React from "react";
 import { ArticleCard } from "./ArticleCard";
 import NFTCollectionFactory from "@/Tests/Factories/Nfts/NFTCollectionFactory";
 import { render, screen } from "@/Tests/testing-library";
+import FeaturedCollectionData = App.Data.Articles.FeaturedCollectionData;
 
 describe("ArticleCard", () => {
-    it("should render ", () => {
-        const collections = new NFTCollectionFactory().withImage().createMany(3);
+    const collections = new NFTCollectionFactory().withImage().createMany(3) as FeaturedCollectionData[];
 
-        render(
-            <ArticleCard
-                article={{
-                    title: "title",
-                }}
-                collections={collections}
-            />,
-        );
+    const article = {
+        id: 1,
+        slug: "title",
+        title: "title",
+        image: "image",
+        userId: 1,
+        content: "content",
+        category: "news",
+        publishedAt: "23423423",
+        metaDescription: "meta",
+        featuredCollections: collections,
+    };
 
-        expect(screen.getByTestId("ArticleCard")).toBeInTheDocument();
-    });
-    it("should render if collections have no image", () => {
-        const collections = new NFTCollectionFactory().withoutImage().createMany(3);
-
-        render(
-            <ArticleCard
-                article={{
-                    title: "title",
-                }}
-                collections={collections}
-            />,
-        );
+    it("should render", () => {
+        render(<ArticleCard article={article} />);
 
         expect(screen.getByTestId("ArticleCard")).toBeInTheDocument();
     });
--- resources/js/Components/Articles/ArticleListItem/ArticleListItem.test.tsx
+++ resources/js/Components/Articles/ArticleListItem/ArticleListItem.test.tsx
@@ -2,33 +2,26 @@ import React from "react";
 import { ArticleListItem } from "./ArticleListItem";
 import NFTCollectionFactory from "@/Tests/Factories/Nfts/NFTCollectionFactory";
 import { render, screen } from "@/Tests/testing-library";
+import FeaturedCollectionData = App.Data.Articles.FeaturedCollectionData;
 
 describe("ArticleListItem", () => {
+    const collections = new NFTCollectionFactory().withImage().createMany(3) as FeaturedCollectionData[];
+
+    const article = {
+        id: 1,
+        slug: "title",
+        title: "title",
+        image: "image",
+        userId: 1,
+        content: "content",
+        category: "news",
+        publishedAt: "23423423",
+        metaDescription: "meta",
+        featuredCollections: collections,
+    };
+
     it("should render ", () => {
-        const collections = new NFTCollectionFactory().withImage().createMany(3);
-
-        render(
-            <ArticleListItem
-                article={{
-                    title: "title",
-                }}
-                collections={collections}
-            />,
-        );
-
-        expect(screen.getByTestId("ArticleListItem")).toBeInTheDocument();
-    });
-    it("should render if collections have no image", () => {
-        const collections = new NFTCollectionFactory().withoutImage().createMany(3);
-
-        render(
-            <ArticleListItem
-                article={{
-                    title: "title",
-                }}
-                collections={collections}
-            />,
-        );
+        render(<ArticleListItem article={article} />);
 
         expect(screen.getByTestId("ArticleListItem")).toBeInTheDocument();
     });
