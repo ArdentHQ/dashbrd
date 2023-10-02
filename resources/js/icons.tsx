990e1d329c53ba89a9466c6798a7c95778c05e21
ItsANameToo <itsanametoo@protonmail.com>
2023-10-02 12:27:44 +0200

    refactor: add opensea icon to footer

--- resources/js/icons.tsx
+++ resources/js/icons.tsx
@@ -16,6 +16,7 @@ import { ReactComponent as GitHub } from "@icons/brands/github.svg";
 import { ReactComponent as Mnemonic } from "@icons/brands/mnemonic.svg";
 import { ReactComponent as Moralis } from "@icons/brands/moralis.svg";
 import { ReactComponent as Opensea } from "@icons/brands/opensea.svg";
+import { ReactComponent as OpenseaCircle } from "@icons/brands/opensea-circle.svg";
 import { ReactComponent as Polygonscan } from "@icons/brands/polygonscan.svg";
 import { ReactComponent as TwitterLg } from "@icons/brands/twitter-lg.svg";
 import { ReactComponent as Twitter } from "@icons/brands/twitter.svg";
@@ -120,6 +121,7 @@ export const SvgCollection = {
     TwitterLg,
     Uniswap,
     Opensea,
+    OpenseaCircle,
     OpenseaColor,
     Blur,
     Calendar,
