4d4494e482a3dc3ffa9533acda72063acd5bdda0
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-05 10:17:22 -0600

    update icon

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -19,7 +19,7 @@ class RoleResource extends Resource
 {
     protected static ?string $model = Role::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-key';
 
     public static function form(Form $form): Form
     {
