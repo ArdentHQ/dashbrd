12d8c36d1c215ab69f275c2bfbf83c3d1cb71bae
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-10-06 08:06:57 -0500

    feat: dark mode for banner´

--- resources/js/Pages/Galleries/Components/GalleryGuestBanner/index.tsx
+++ resources/js/Pages/Galleries/Components/GalleryGuestBanner/index.tsx
@@ -14,13 +14,13 @@ const GalleryGuestBanner = ({ initialized, connecting, onClick }: Properties): J
     const { isMdAndAbove } = useBreakpoint();
 
     return (
-        <div className="gallery-guest-banner mx-6 mt-4 flex flex-col gap-3 rounded-xl border border-theme-secondary-300 bg-cover bg-center p-6 backdrop-blur sm:mx-8 md:flex-row md:items-center md:justify-between md:gap-4 md:bg-left 2xl:mx-0">
+        <div className="gallery-guest-banner mx-6 mt-4 flex flex-col gap-3 rounded-xl border border-theme-secondary-300 bg-cover bg-center p-6 backdrop-blur sm:mx-8 md:flex-row md:items-center md:justify-between md:gap-4 md:bg-left 2xl:mx-0 dark:border-theme-dark-700">
             <div className="flex w-full flex-col gap-0.5 md:h-14 md:w-fit md:justify-center">
-                <h3 className="text-center text-xl font-medium capitalize leading-7 text-theme-secondary-900 md:text-left md:text-2xl">
+                <h3 className="text-center text-xl font-medium capitalize leading-7 text-theme-secondary-900 md:text-left md:text-2xl dark:text-theme-dark-50">
                     {t("pages.galleries.guest_banner.title")}{" "}
-                    <span className="block text-theme-primary-600 sm:inline-block">{t("common.nft_gallery")}</span>
+                    <span className="block text-theme-primary-600 sm:inline-block dark:text-theme-primary-400">{t("common.nft_gallery")}</span>
                 </h3>
-                <p className="text-center text-xs font-medium text-theme-secondary-700 md:text-left md:text-sm">
+                <p className="text-center text-xs font-medium text-theme-secondary-700 md:text-left md:text-sm dark:text-theme-dark-200">
                     {t("pages.galleries.guest_banner.subtitle")}
                 </p>
             </div>
@@ -28,7 +28,7 @@ const GalleryGuestBanner = ({ initialized, connecting, onClick }: Properties): J
             <div className="flex h-fit justify-center">
                 <Button
                     icon="Plus"
-                    className="justify-center·py-2·sm:w-fit·sm:px-6"
+                    className="justify-center·py-2·sm:w-fit·sm:px-6 dark:button-secondary-light bg-theme-secondary-100 md:button-light"
                     disabled={connecting || !initialized}
                     onClick={onClick}
                     variant={isMdAndAbove ? "secondary" : "primary"}
