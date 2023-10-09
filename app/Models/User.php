ec656703f19c117cdf56845637e7e3c29ded163a
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-09 13:12:24 -0600

    fix broken queries

--- app/Models/User.php
+++ app/Models/User.php
@@ -94,8 +94,8 @@ class User extends Authenticatable implements FilamentUser, HasName
     public function collections(): Builder
     {
         return Collection::join('nfts', 'collections.id', '=', 'nfts.collection_id')
-            ->join('wallets', 'wallets.id', '=', 'nfts.wallet_id')
-            ->where('wallets.user_id', $this->id);
+            ->where('nfts.wallet_id', $this->wallet_id)
+            ->groupBy('collections.id');
     }
 
     /**
