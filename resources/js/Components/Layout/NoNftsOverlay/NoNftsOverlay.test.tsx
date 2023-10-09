2be49e1151cf1720e14724e142cbb82bf42fe3a4
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-10-09 06:48:28 -0500

    fix: define DarkModeContext for NoNftsOverlay

--- resources/js/Components/Layout/NoNftsOverlay/NoNftsOverlay.test.tsx
+++ resources/js/Components/Layout/NoNftsOverlay/NoNftsOverlay.test.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { NoNftsOverlay } from "@/Components/Layout/NoNftsOverlay";
 import { render, screen } from "@/Tests/testing-library";
+import * as useDarkModeContext from "@/Contexts/DarkModeContex";
 
 describe("NoNftsOverlay", () => {
+    beforeAll(() => {
+        vi.spyOn(useDarkModeContext, "useDarkModeContext").mockReturnValue({ isDark: false, toggleDarkMode: vi.fn() });
+    });
+
+    afterAll(() => {
+        vi.restoreAllMocks();
+    });
+
     it("shows the overlay", () => {
         render(<NoNftsOverlay show={true} />);
 
