26b0bb1fea84e9a8640d144d1233d1e87b36e2c0
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2023-10-11 12:28:56 -0500

    fix: correct colors for create gallery button (#211)

--- resources/js/Pages/Galleries/Components/GalleryGuestBanner/index.tsx
+++ resources/js/Pages/Galleries/Components/GalleryGuestBanner/index.tsx
@@ -30,7 +30,7 @@ const GalleryGuestBanner = ({ initialized, connecting, onClick }: Properties): J
             <div className="flex h-fit justify-center">
                 <Button
                     icon="Plus"
-                    className="justify-center·py-2·sm:w-fit·sm:px-6 dark:button-secondary-light md:button-light bg-theme-secondary-100"
+                    className="dark:button-secondary-light md:button-light justify-center py-2 sm:w-fit sm:px-6"
                     disabled={connecting || !initialized}
                     onClick={onClick}
                     variant={isMdAndAbove ? "secondary" : "primary"}
