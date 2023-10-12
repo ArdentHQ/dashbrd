fcc9536fe169a31687bac6446fdc1e646a04840f
Patricio Marroquin <55117912+patricio0312rev@users.noreply.github.com>
2023-10-12 03:15:58 -0500

    feat: dark mode for gallery details page (#208)

--- resources/js/Components/Buttons/LikeButton.tsx
+++ resources/js/Components/Buttons/LikeButton.tsx
@@ -8,8 +8,8 @@ export const LikeButton = ({ children, className, ...properties }: LikeButtonPro
         variant="bordered"
         className={cn(className, "button-like")}
         {...properties}
-        iconClass="text-theme-primary-900"
+        iconClass="text-theme-primary-900 dark:text-theme-dark-300"
     >
-        <span className="text-sm font-medium text-theme-secondary-700">{children}</span>
+        <span className="text-sm font-medium text-theme-secondary-700 dark:text-theme-dark-200">{children}</span>
     </Button>
 );
