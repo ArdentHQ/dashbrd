24b6573634b8f0dc6a27b8218f493f9fc3aa662f
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-12 16:14:02 -0600

    wip

--- resources/js/Pages/Articles/Show.tsx
+++ resources/js/Pages/Articles/Show.tsx
@@ -1,8 +1,13 @@
+import { Heading } from "@/Components/Heading";
 import { DefaultLayout } from "@/Layouts/DefaultLayout";
 
 const ArticlesShow = ({ article }: { article: App.Data.Articles.ArticleData }): JSX.Element => (
     <DefaultLayout>
-        <div className="mx-6 sm:mx-8 2xl:mx-0">{article.title}</div>
+        <div className="article-wrapper rounded-xl border border-theme-secondary-300 pb-6 pt-8 sm:px-24">
+            <article>
+                <Heading level={1}>{article.title}</Heading>
+            </article>
+        </div>
     </DefaultLayout>
 );
 
