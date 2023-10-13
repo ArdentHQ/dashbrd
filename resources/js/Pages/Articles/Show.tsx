124d5d0cd983aae47907bbe9c8b97ab60c2bdad7
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-13 10:47:33 -0600

    add header white background

--- resources/js/Pages/Articles/Show.tsx
+++ resources/js/Pages/Articles/Show.tsx
@@ -4,18 +4,22 @@ import { DefaultLayout } from "@/Layouts/DefaultLayout";
 
 const ArticlesShow = ({ article }: { article: App.Data.Articles.ArticleData }): JSX.Element => (
     <DefaultLayout>
-        <article className="article-wrapper -mt-6 px-8 pb-6 pt-8 sm:-mt-8 md:px-24 lg:mx-8 lg:mt-0 lg:overflow-hidden lg:rounded-xl lg:border lg:border-theme-secondary-300">
-            <div className="border-b border-theme-secondary-400 pb-3">
-                <Heading level={1}>{article.title}</Heading>
+        <article className="article-wrapper -mt-6 pb-6 sm:-mt-8  lg:mx-8 lg:mt-0 lg:overflow-hidden lg:rounded-xl lg:border lg:border-theme-secondary-300">
+            <div className="bg-white/20 px-8 pt-8 md:px-24">
+                <div className="border-b border-theme-secondary-400  pb-3">
+                    <Heading level={1}>{article.title}</Heading>
+                </div>
             </div>
 
-            <div className="mt-6 aspect-video overflow-hidden rounded-lg bg-theme-secondary-300">
-                <Img
-                    className="h-full w-full rounded-lg object-cover"
-                    wrapperClassName="h-full [&>span]:h-full bg-white"
-                    alt={article.title}
-                    src={article.image}
-                />
+            <div className="mt-6 px-8 md:px-24 ">
+                <div className="aspect-video overflow-hidden rounded-lg bg-theme-secondary-300 ">
+                    <Img
+                        className="h-full w-full rounded-lg object-cover"
+                        wrapperClassName="h-full [&>span]:h-full bg-white"
+                        alt={article.title}
+                        src={article.image}
+                    />
+                </div>
             </div>
         </article>
     </DefaultLayout>
