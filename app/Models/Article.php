c533595e9d3ac9598e24dcd5bcaf3489842552bf
Shahin Safaraliyev <shahin@ardenthq.com>
2023-10-13 14:37:01 +0400

    wip

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\DB;
 use Spatie\Image\Manipulations;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
