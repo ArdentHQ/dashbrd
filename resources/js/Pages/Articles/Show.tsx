15f72ecede8075f447e641921740b5b85843f17c
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-13 10:43:35 -0600

    add background and adjust padding/margin

--- resources/js/Pages/Articles/Show.tsx
+++ resources/js/Pages/Articles/Show.tsx
@@ -4,22 +4,20 @@ import { DefaultLayout } from "@/Layouts/DefaultLayout";
 
 const ArticlesShow = ({ article }: { article: App.Data.Articles.ArticleData }): JSX.Element => (
     <DefaultLayout>
-        <div className="mx-8 -mt-6 pb-6 pt-8 sm:-mt-8 md:px-24 lg:mt-0 lg:overflow-hidden lg:rounded-xl lg:border lg:border-theme-secondary-300">
-            <article>
-                <div className="border-b border-theme-secondary-400 pb-3">
-                    <Heading level={1}>{article.title}</Heading>
-                </div>
+        <article className="article-wrapper -mt-6 px-8 pb-6 pt-8 sm:-mt-8 md:px-24 lg:mx-8 lg:mt-0 lg:overflow-hidden lg:rounded-xl lg:border lg:border-theme-secondary-300">
+            <div className="border-b border-theme-secondary-400 pb-3">
+                <Heading level={1}>{article.title}</Heading>
+            </div>
 
-                <div className="mt-6 aspect-video overflow-hidden rounded-lg bg-theme-secondary-300">
-                    <Img
-                        className="h-full w-full rounded-lg object-cover"
-                        wrapperClassName="h-full [&>span]:h-full bg-white"
-                        alt={article.title}
-                        src={article.image}
-                    />
-                </div>
-            </article>
-        </div>
+            <div className="mt-6 aspect-video overflow-hidden rounded-lg bg-theme-secondary-300">
+                <Img
+                    className="h-full w-full rounded-lg object-cover"
+                    wrapperClassName="h-full [&>span]:h-full bg-white"
+                    alt={article.title}
+                    src={article.image}
+                />
+            </div>
+        </article>
     </DefaultLayout>
 );
 
