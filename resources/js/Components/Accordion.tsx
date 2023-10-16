61af7843d1757bc7e1c933786b48624c37c50c33
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-10-16 13:09:58 -0500

    feat: dark mode for create gallery accordion

--- resources/js/Components/Accordion.tsx
+++ resources/js/Components/Accordion.tsx
@@ -27,16 +27,19 @@ export const AccordionItem = ({ title, children }: AccordionItemProperties): JSX
                     setOpen(!isOpen);
                 }}
             >
-                <div className="transition-default mx-2 my-1.5 flex min-w-0 items-center rounded-xl p-2 group-hover:bg-theme-secondary-100 sm:mx-3 sm:p-3">
+                <div className="transition-default mx-2 my-1.5 flex min-w-0 items-center rounded-xl p-2 group-hover:bg-theme-secondary-100 dark:group-hover:bg-theme-dark-800 sm:mx-3 sm:p-3">
                     <div className="min-w-0 flex-1">{title}</div>
 
                     <div className="ml-2 flex h-6 w-6 items-center justify-center">
                         <Icon
                             name="ChevronDownSmall"
                             size="sm"
-                            className={cn("transform text-theme-secondary-900 transition duration-100", {
-                                "rotate-180": isOpen,
-                            })}
+                            className={cn(
+                                "transform text-theme-secondary-900 transition duration-100 dark:text-theme-dark-50",
+                                {
+                                    "rotate-180": isOpen,
+                                },
+                            )}
                         />
                     </div>
                 </div>
@@ -66,7 +69,7 @@ const HeadOnlyAccordionItem = ({ title }: Pick<AccordionItemProperties, "title">
 export const Accordion = ({ children }: { children: React.ReactNode }): JSX.Element => (
     <div
         data-testid="Accordion"
-        className="divide-y divide-theme-secondary-300 rounded-xl border border-theme-secondary-300 py-2"
+        className="divide-y divide-theme-secondary-300 rounded-xl border border-theme-secondary-300 py-2 dark:divide-theme-dark-700 dark:border-theme-dark-700"
     >
         {children}
     </div>
