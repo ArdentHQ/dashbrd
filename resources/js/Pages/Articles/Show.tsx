c63144ec5bcaed29f0b4753c7db7a05cd90bdcdf
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-17 13:08:43 -0600

    article show

--- resources/js/Pages/Articles/Show.tsx
+++ resources/js/Pages/Articles/Show.tsx
@@ -1,11 +1,12 @@
+import { ArticleShare } from "./Components/ArticleShare";
 import { Heading } from "@/Components/Heading";
 import { Img } from "@/Components/Image";
 import { DefaultLayout } from "@/Layouts/DefaultLayout";
 
 const ArticlesShow = ({ article }: { article: App.Data.Articles.ArticleData }): JSX.Element => (
     <DefaultLayout>
-        <article className=" w-full lg:px-8 2xl:px-0">
-            <div className="article-wrapper -mt-6 px-8 pb-6 sm:-mt-8 md:px-24 lg:mt-0 lg:overflow-hidden lg:rounded-xl lg:border lg:border-theme-secondary-300 lg:px-0">
+        <article className="w-full lg:px-8 2xl:px-0">
+            <div className="article-wrapper -mt-6 px-8 sm:-mt-8 md:px-24 lg:mt-0 lg:rounded-xl lg:border lg:border-theme-secondary-300 lg:px-0">
                 <div className="mx-auto bg-white/20 pt-8 md:max-w-[768px] xl:max-w-[1000px]">
                     <div className="border-b-2 border-theme-secondary-400 pb-3">
                         <Heading level={1}>{article.title}</Heading>
@@ -21,6 +22,26 @@ const ArticlesShow = ({ article }: { article: App.Data.Articles.ArticleData }): 
                             src={article.image}
                         />
                     </div>
+
+                    <div className="relative mt-6 pb-6">
+                        <div className="top-8 hidden md:sticky md:block">
+                            <ArticleShare
+                                article={article}
+                                className="absolute -ml-[68px] flex flex-col space-y-2"
+                            />
+                        </div>
+
+                        <div>
+                            {/* Content here */}
+                            {Array.from({ length: 100 }).map((_, index) => (
+                                <p key={index}>Test scroll, replace with real content {index}</p>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                <div className="-mx-6 flex border-b border-t border-theme-secondary-300 px-6 py-4 sm:-mx-8 sm:px-8 md:mx-0 md:rounded-xl md:border md:px-6 lg:rounded-none lg:border-x-0 lg:border-b-0">
+                    Meta Info
                 </div>
             </div>
         </article>
