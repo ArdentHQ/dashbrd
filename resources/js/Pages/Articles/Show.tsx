33a4d54f9868284054541a5a129c03c3d789127a
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-17 09:22:40 -0600

    padding adjustments

--- resources/js/Pages/Articles/Show.tsx
+++ resources/js/Pages/Articles/Show.tsx
@@ -4,21 +4,23 @@ import { DefaultLayout } from "@/Layouts/DefaultLayout";
 
 const ArticlesShow = ({ article }: { article: App.Data.Articles.ArticleData }): JSX.Element => (
     <DefaultLayout>
-        <article className="article-wrapper -mt-6 pb-6 sm:-mt-8  lg:mx-8 lg:mt-0 lg:overflow-hidden lg:rounded-xl lg:border lg:border-theme-secondary-300">
-            <div className="bg-white/20 px-8 pt-8 md:px-24">
-                <div className="border-b-2 border-theme-secondary-400  pb-3">
-                    <Heading level={1}>{article.title}</Heading>
+        <article className="article-wrapper w-full px-8 md:px-24 lg:px-8 2xl:px-0">
+            <div className=" -mt-6 pb-6 sm:-mt-8 lg:mt-0 lg:overflow-hidden lg:rounded-xl lg:border lg:border-theme-secondary-300">
+                <div className="mx-auto bg-white/20 pt-8 md:max-w-[768px] xl:max-w-[1000px]">
+                    <div className="border-b-2 border-theme-secondary-400 pb-3">
+                        <Heading level={1}>{article.title}</Heading>
+                    </div>
                 </div>
-            </div>
 
-            <div className="mt-6 px-8 md:px-24 ">
-                <div className="aspect-video overflow-hidden rounded-lg bg-theme-secondary-300 ">
-                    <Img
-                        className="h-full w-full rounded-lg object-cover"
-                        wrapperClassName="h-full [&>span]:h-full bg-white"
-                        alt={article.title}
-                        src={article.image}
-                    />
+                <div className="mx-auto mt-6 md:max-w-[768px] xl:max-w-[1000px]">
+                    <div className="aspect-video overflow-hidden rounded-lg bg-theme-secondary-300 ">
+                        <Img
+                            className="h-full w-full rounded-lg object-cover"
+                            wrapperClassName="h-full [&>span]:h-full bg-white"
+                            alt={article.title}
+                            src={article.image}
+                        />
+                    </div>
                 </div>
             </div>
         </article>
