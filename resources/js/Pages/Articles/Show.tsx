fa81270a3fcfb7173c4651f3371f1135754dddbb
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-17 13:19:37 -0600

    fix padding on mobile

--- resources/js/Pages/Articles/Show.tsx
+++ resources/js/Pages/Articles/Show.tsx
@@ -6,7 +6,7 @@ import { DefaultLayout } from "@/Layouts/DefaultLayout";
 const ArticlesShow = ({ article }: { article: App.Data.Articles.ArticleData }): JSX.Element => (
     <DefaultLayout>
         <article className="w-full lg:px-8 2xl:px-0">
-            <div className="article-wrapper -mt-6 px-8 sm:-mt-8 md:px-24 lg:mt-0 lg:rounded-xl lg:border lg:border-theme-secondary-300 lg:px-0">
+            <div className="article-wrapper -mt-6 px-6 sm:-mt-8 sm:px-8 md:px-24 lg:mt-0 lg:rounded-xl lg:border lg:border-theme-secondary-300 lg:px-0">
                 <div className="mx-auto bg-white/20 pt-8 md:max-w-[768px] xl:max-w-[1000px]">
                     <div className="border-b-2 border-theme-secondary-400 pb-3">
                         <Heading level={1}>{article.title}</Heading>
@@ -40,7 +40,7 @@ const ArticlesShow = ({ article }: { article: App.Data.Articles.ArticleData }): 
                     </div>
                 </div>
 
-                <div className="-mx-6 flex flex-col  border-b border-t border-theme-secondary-300 px-6 py-4 sm:-mx-8 sm:px-8 md:mx-0 md:rounded-xl md:border md:px-6 lg:rounded-none lg:border-x-0 lg:border-b-0">
+                <div className="-mx-6 flex flex-col border-b border-t border-theme-secondary-300 px-6 py-4 sm:-mx-8 sm:px-8 md:mx-0 md:rounded-xl md:border md:px-6 lg:rounded-none lg:border-x-0 lg:border-b-0">
                     <div className="flex justify-between">
                         <div className="flex">
                             <div>Author (TBD)</div>
