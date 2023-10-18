09bdf23980cd2f02395ba92b9465042e3deb5538
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-18 11:31:20 -0600

    most popular articles ticle

--- resources/js/Pages/Articles/Show.tsx
+++ resources/js/Pages/Articles/Show.tsx
@@ -12,78 +12,76 @@ interface Properties {
     popularArticles: App.Data.Articles.ArticleData[];
 }
 
-const ArticlesShow = ({ article, popularArticles }: Properties): JSX.Element => {
-    console.log({ popularArticles });
-    return (
-        <DefaultLayout>
-            <div className="w-full lg:px-8 2xl:px-0">
-                <article className="article-wrapper -mt-6 px-6 sm:-mt-8 sm:px-8 md:px-24 lg:mt-0 lg:rounded-xl lg:border lg:border-theme-secondary-300 lg:px-0">
-                    <div className="mx-auto bg-white/20 pt-8 md:max-w-[768px] xl:max-w-[1000px]">
-                        <div className="border-b-2 border-theme-secondary-400 pb-3">
-                            <Heading level={1}>{article.title}</Heading>
-                        </div>
+const ArticlesShow = ({ article, popularArticles }: Properties): JSX.Element => (
+    <DefaultLayout>
+        <div className="w-full lg:px-8 2xl:px-0">
+            <article className="article-wrapper -mt-6 px-6 sm:-mt-8 sm:px-8 md:px-24 lg:mt-0 lg:rounded-xl lg:border lg:border-theme-secondary-300 lg:px-0">
+                <div className="mx-auto bg-white/20 pt-8 md:max-w-[768px] xl:max-w-[1000px]">
+                    <div className="border-b-2 border-theme-secondary-400 pb-3">
+                        <Heading level={1}>{article.title}</Heading>
+                    </div>
+                </div>
+
+                <div className="mx-auto mt-6 md:max-w-[768px] xl:max-w-[1000px]">
+                    <div className="aspect-video overflow-hidden rounded-lg bg-theme-secondary-300 ">
+                        <Img
+                            className="h-full w-full rounded-lg object-cover"
+                            wrapperClassName="h-full [&>span]:h-full bg-white"
+                            alt={article.title}
+                            src={article.image}
+                        />
                     </div>
 
-                    <div className="mx-auto mt-6 md:max-w-[768px] xl:max-w-[1000px]">
-                        <div className="aspect-video overflow-hidden rounded-lg bg-theme-secondary-300 ">
-                            <Img
-                                className="h-full w-full rounded-lg object-cover"
-                                wrapperClassName="h-full [&>span]:h-full bg-white"
-                                alt={article.title}
-                                src={article.image}
+                    <div className="relative mt-6 pb-6">
+                        <div className="top-8 hidden md:sticky md:block">
+                            <ArticleShare
+                                article={article}
+                                className="absolute -ml-[68px] flex flex-col space-y-2"
                             />
                         </div>
 
-                        <div className="relative mt-6 pb-6">
-                            <div className="top-8 hidden md:sticky md:block">
-                                <ArticleShare
-                                    article={article}
-                                    className="absolute -ml-[68px] flex flex-col space-y-2"
-                                />
-                            </div>
+                        <div>
+                            {/* Content here */}
+                            {Array.from({ length: 100 }).map((_, index) => (
+                                <p key={index}>Test scroll, replace with real content {index}</p>
+                            ))}
+                        </div>
+                    </div>
+                </div>
 
-                            <div>
-                                {/* Content here */}
-                                {Array.from({ length: 100 }).map((_, index) => (
-                                    <p key={index}>Test scroll, replace with real content {index}</p>
-                                ))}
-                            </div>
+                <div className="-mx-6 flex flex-col border-b border-t border-theme-secondary-300 px-6 py-4 sm:-mx-8 sm:px-8 md:mx-0 md:rounded-xl md:border md:px-6 lg:rounded-none lg:border-x-0 lg:border-b-0">
+                    <div className="flex items-center justify-between">
+                        <div className="flex overflow-auto">
+                            <ArticleAuthor article={article} />
+
+                            <div className="mx-6 border-r border-theme-secondary-300"></div>
+
+                            <ArticleDate article={article} />
+                        </div>
+
+                        <div className="hidden md:block">
+                            <ArticleCopy article={article} />
                         </div>
                     </div>
 
-                    <div className="-mx-6 flex flex-col border-b border-t border-theme-secondary-300 px-6 py-4 sm:-mx-8 sm:px-8 md:mx-0 md:rounded-xl md:border md:px-6 lg:rounded-none lg:border-x-0 lg:border-b-0">
-                        <div className="flex items-center justify-between">
-                            <div className="flex overflow-auto">
-                                <ArticleAuthor article={article} />
-
-                                <div className="mx-6 border-r border-theme-secondary-300"></div>
-
-                                <ArticleDate article={article} />
-                            </div>
-
-                            <div className="hidden md:block">
-                                <ArticleCopy article={article} />
-                            </div>
+                    <div className="mt-4 flex justify-between border-t border-dashed border-theme-secondary-400 pt-4 md:hidden">
+                        <div>
+                            <ArticleShare
+                                article={article}
+                                className="flex space-x-2"
+                            />
                         </div>
-                        <div className="mt-4 flex justify-between border-t border-dashed border-theme-secondary-400 pt-4 md:hidden">
-                            <div>
-                                <ArticleShare
-                                    article={article}
-                                    className="flex space-x-2"
-                                />
-                            </div>
 
-                            <div>
-                                <ArticleCopy article={article} />
-                            </div>
+                        <div>
+                            <ArticleCopy article={article} />
                         </div>
                     </div>
-                </article>
-            </div>
+                </div>
+            </article>
+        </div>
 
-            <ArticlesGridScrollable articles={popularArticles} />
-        </DefaultLayout>
-    );
-};
+        <ArticlesGridScrollable articles={popularArticles} />
+    </DefaultLayout>
+);
 
 export default ArticlesShow;
--- resources/js/Pages/Collections/Components/Articles/ArticlesGridScrollable.tsx
+++ resources/js/Pages/Collections/Components/Articles/ArticlesGridScrollable.tsx
@@ -1,16 +1,40 @@
+import { useTranslation } from "react-i18next";
 import { ArticleCard } from "@/Components/Articles/ArticleCard";
+import { IconButton } from "@/Components/Buttons";
+import { Heading } from "@/Components/Heading";
 
-export const ArticlesGridScrollable = ({ articles }: { articles: App.Data.Articles.ArticleData[] }): JSX.Element => (
-    <div className="xl:-mx-1.5 xl:px-8 2xl:px-0">
-        <div className=" flex max-w-full snap-x scroll-px-[26px] overflow-x-auto px-[26px]  xl:px-0">
-            {articles.map((article) => (
-                <div
-                    key={article.id}
-                    className="w-full flex-shrink-0 snap-start px-1.5 sm:w-1/2 md-lg:w-1/3 xl:w-1/4"
-                >
-                    <ArticleCard article={article} />
+export const ArticlesGridScrollable = ({ articles }: { articles: App.Data.Articles.ArticleData[] }): JSX.Element => {
+    const { t } = useTranslation();
+
+    return (
+        <div className="mt-8">
+            <div className="flex w-full items-center justify-between px-6 sm:px-8 lg:px-8 2xl:px-0">
+                <Heading level={2}>
+                    <span className="text-theme-primary-600">{t("common.most_popular")}</span> {t("common.articles")}
+                </Heading>
+
+                <div className="hidden space-x-3 md:flex xl:hidden">
+                    <IconButton icon="ChevronLeftSmall" />
+
+                    <IconButton
+                        disabled
+                        icon="ChevronRightSmall"
+                    />
                 </div>
-            ))}
+            </div>
+
+            <div className="mt-4 xl:-mx-1.5 xl:px-8 2xl:px-0">
+                <div className=" flex max-w-full snap-x scroll-px-[18px] overflow-x-auto px-[18px] sm:scroll-px-[26px] sm:px-[26px] xl:px-0">
+                    {articles.map((article) => (
+                        <div
+                            key={article.id}
+                            className="w-full flex-shrink-0 snap-start px-1.5 sm:w-1/2 md-lg:w-1/3 xl:w-1/4"
+                        >
+                            <ArticleCard article={article} />
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
-    </div>
-);
+    );
+};
