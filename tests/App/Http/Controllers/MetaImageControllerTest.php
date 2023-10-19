5ef6fd82191f12ba87ea2cf31c104c6f3108bf5c
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-19 13:04:38 -0600

    Update MetaImageControllerTest.php

--- tests/App/Http/Controllers/MetaImageControllerTest.php
+++ tests/App/Http/Controllers/MetaImageControllerTest.php
@@ -24,62 +24,127 @@ afterEach(function () {
     File::deleteDirectory($sourceDir);
 });
 
-it('removes deprecated existing images for the gallery when pruning', function () {
+it('skips image generation if file already exist', function () {
     emptyMetaImagesFolder();
 
-    $gallery = Gallery::factory()->create();
+    Gallery::truncate();
 
-    $nft = Nft::factory()->create();
+    $gallery = Gallery::factory()->create([
+        'name' => 'Test Gallery',
+    ]);
 
-    $gallery->nfts()->attach($nft, ['order_index' => 0]);
-
-    // Pre-existing image
-    copy(base_path('tests/fixtures/page-screenshot.png'), storage_path('meta/galleries/'.$gallery->slug.'_test.png'));
-
-    // For other gallery
-    copy(base_path('tests/fixtures/page-screenshot.png'), storage_path('meta/galleries/other-slug_test.png'));
-
-    $directory = storage_path('meta/galleries/');
+    // md5 hash hardcoded here, comes from gallery name and amount and order of nfts (zero in this case)
+    copy(base_path('tests/fixtures/page-screenshot.png'), storage_path('meta/galleries/test-gallery_2d84f6a1884d56f30d33675aeeaa2aab.png'));
 
     $this
         ->mock(Browsershot::class)
-        ->shouldReceive('url')
-        ->with(route('galleries.view', ['gallery' => $gallery->slug]))
-        ->once()
-        ->andReturnSelf()
-        ->shouldReceive('windowSize')
-        ->once()
-        ->andReturnSelf()
-        ->shouldReceive('timeout')
-        ->with(60)
-        ->once()
-        ->andReturnSelf()
-        ->shouldReceive('waitForFunction')
-        ->once()
-        ->andReturnSelf()
-        ->shouldReceive('setNodeBinary')
-        ->once()
-        ->andReturnSelf()
-        ->shouldReceive('setNpmBinary')
-        ->once()
-        ->andReturnSelf()
-        // Mock save method implementation
-        ->shouldReceive('save')
-        ->once()
-        ->andReturnUsing(function ($test) {
-            // Emulate stored screenshot
-            copy(base_path('tests/fixtures/page-screenshot.png'), $test);
-        });
+        ->shouldNotReceive('url');
 
-    $this->get(route('galleries.meta-image', ['gallery' => $gallery->slug]))->assertOk();
-
-    $directory = storage_path('meta/galleries/');
-
-    expect(glob($directory.'*'))->toHaveCount(3);
-
-    Artisan::call('prune-meta-images');
-
-    expect(glob($directory.'*'))->toHaveCount(1);
-
-    expect(glob($directory.$gallery->slug.'_*'))->toHaveCount(1);
+    $this
+        ->get(route('galleries.meta-image', ['gallery' => $gallery->slug]))
+        ->assertOk();
 });
+
+// it('generates an image', function () {
+//     emptyMetaImagesFolder();
+
+//     $gallery = Gallery::factory()->create();
+
+//     $this
+//         ->mock(Browsershot::class)
+//         ->shouldReceive('url')
+//         ->with(route('galleries.view', ['gallery' => $gallery->slug]))
+//         ->once()
+//         ->andReturnSelf()
+//         ->shouldReceive('windowSize')
+//         ->once()
+//         ->andReturnSelf()
+//         ->shouldReceive('timeout')
+//         ->with(60)
+//         ->once()
+//         ->andReturnSelf()
+//         ->shouldReceive('waitForFunction')
+//         ->once()
+//         ->andReturnSelf()
+//         ->shouldReceive('setNodeBinary')
+//         ->once()
+//         ->andReturnSelf()
+//         ->shouldReceive('setNpmBinary')
+//         ->once()
+//         ->andReturnSelf()
+//         // Mock save method implementation
+//         ->shouldReceive('save')
+//         ->once()
+//         ->andReturnUsing(function ($test) {
+//             // Emulate stored screenshot
+//             copy(base_path('tests/fixtures/page-screenshot.png'), $test);
+//         });
+
+//     $this->get(route('galleries.meta-image', ['gallery' => $gallery->slug]))->assertOk();
+
+//     $directory = storage_path('meta/galleries/');
+
+//     $files = glob($directory.$gallery->slug.'*');
+
+//     expect($files)->toHaveCount(1);
+// });
+
+// it('removes deprecated existing images for the gallery when pruning', function () {
+//     emptyMetaImagesFolder();
+
+//     $gallery = Gallery::factory()->create();
+
+//     $nft = Nft::factory()->create();
+
+//     $gallery->nfts()->attach($nft, ['order_index' => 0]);
+
+//     // Pre-existing image
+//     copy(base_path('tests/fixtures/page-screenshot.png'), storage_path('meta/galleries/'.$gallery->slug.'_test.png'));
+
+//     // For other gallery
+//     copy(base_path('tests/fixtures/page-screenshot.png'), storage_path('meta/galleries/other-slug_test.png'));
+
+//     $directory = storage_path('meta/galleries/');
+
+//     $this
+//         ->mock(Browsershot::class)
+//         ->shouldReceive('url')
+//         ->with(route('galleries.view', ['gallery' => $gallery->slug]))
+//         ->once()
+//         ->andReturnSelf()
+//         ->shouldReceive('windowSize')
+//         ->once()
+//         ->andReturnSelf()
+//         ->shouldReceive('timeout')
+//         ->with(60)
+//         ->once()
+//         ->andReturnSelf()
+//         ->shouldReceive('waitForFunction')
+//         ->once()
+//         ->andReturnSelf()
+//         ->shouldReceive('setNodeBinary')
+//         ->once()
+//         ->andReturnSelf()
+//         ->shouldReceive('setNpmBinary')
+//         ->once()
+//         ->andReturnSelf()
+//         // Mock save method implementation
+//         ->shouldReceive('save')
+//         ->once()
+//         ->andReturnUsing(function ($test) {
+//             // Emulate stored screenshot
+//             copy(base_path('tests/fixtures/page-screenshot.png'), $test);
+//         });
+
+//     $this->get(route('galleries.meta-image', ['gallery' => $gallery->slug]))->assertOk();
+
+//     $directory = storage_path('meta/galleries/');
+
+//     expect(glob($directory.'*'))->toHaveCount(3);
+
+//     Artisan::call('prune-meta-images');
+
+//     expect(glob($directory.'*'))->toHaveCount(1);
+
+//     expect(glob($directory.$gallery->slug.'_*'))->toHaveCount(1);
+// });
