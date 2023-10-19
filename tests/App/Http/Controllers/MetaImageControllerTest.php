ca1a0aa17f82645a9cfe7c334aecd7ca63cfc3b1
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-19 12:23:03 -0600

    Update MetaImageControllerTest.php

--- tests/App/Http/Controllers/MetaImageControllerTest.php
+++ tests/App/Http/Controllers/MetaImageControllerTest.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 use App\Models\Gallery;
-use App\Models\Nft;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
 use Spatie\Browsershot\Browsershot;
 
@@ -85,62 +83,4 @@ it('generates an image', function () {
     $files = glob($directory.$gallery->slug.'*');
 
     expect($files)->toHaveCount(1);
-});
-
-it('removes deprecated existing images for the gallery when pruning', function () {
-    $gallery = Gallery::factory()->create();
-
-    $nft = Nft::factory()->create();
-
-    $gallery->nfts()->attach($nft, ['order_index' => 0]);
-
-    // Pre-existing image
-    copy(base_path('tests/fixtures/page-screenshot.png'), storage_path('meta/galleries/'.$gallery->slug.'_test.png'));
-
-    // For other gallery
-    copy(base_path('tests/fixtures/page-screenshot.png'), storage_path('meta/galleries/other-slug_test.png'));
-
-    $directory = storage_path('meta/galleries/');
-
-    $this
-        ->mock(Browsershot::class)
-        ->shouldReceive('url')
-        ->with(route('galleries.view', ['gallery' => $gallery->slug]))
-        ->once()
-        ->andReturnSelf()
-        ->shouldReceive('windowSize')
-        ->once()
-        ->andReturnSelf()
-        ->shouldReceive('timeout')
-        ->with(60)
-        ->once()
-        ->andReturnSelf()
-        ->shouldReceive('waitForFunction')
-        ->once()
-        ->andReturnSelf()
-        ->shouldReceive('setNodeBinary')
-        ->once()
-        ->andReturnSelf()
-        ->shouldReceive('setNpmBinary')
-        ->once()
-        ->andReturnSelf()
-        // Mock save method implementation
-        ->shouldReceive('save')
-        ->once()
-        ->andReturnUsing(function ($test) {
-            // Emulate stored screenshot
-            copy(base_path('tests/fixtures/page-screenshot.png'), $test);
-        });
-
-    $this->get(route('galleries.meta-image', ['gallery' => $gallery->slug]))->assertOk();
-
-    $directory = storage_path('meta/galleries/');
-
-    expect(glob($directory.'*'))->toHaveCount(3);
-
-    Artisan::call('prune-meta-images');
-
-    expect(glob($directory.'*'))->toHaveCount(1);
-
-    expect(glob($directory.$gallery->slug.'_*'))->toHaveCount(1);
 });
