d07a3ea2d4b7536a82e957e31083f5b5a78fd71b
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-24 13:15:13 -0600

    cleanup

--- resources/js/Pages/Articles/Components/ArticleContent.tsx
+++ resources/js/Pages/Articles/Components/ArticleContent.tsx
@@ -1,52 +1,16 @@
 import Markdown from "react-markdown";
 import rehypeRaw from "rehype-raw";
-import html from "rehype-stringify";
-import remarkGfm from "remark-gfm";
-import remark2rehype from "remark-rehype";
-import { unified } from "unified";
 
+import { remarkFigurePlugin } from "@/Utils/Remark/remarkFigurePlugin";
 interface Properties {
     article: App.Data.Articles.ArticleData;
 }
-
-const remarkFigure = () => (tree) => {
-    const nodesToReplace = [];
-    let previous;
-
-    tree.children.forEach((node, index, parent) => {
-        if (node.type === "blockquote") {
-            previous = parent[index - 1];
-
-            if (previous && previous.type === "paragraph" && previous.children[0].type === "image") {
-                nodesToReplace.push({ figure: previous, caption: node, index: index - 1, parent });
-            }
-        }
-    });
-
-    for (const { figure, caption, index, parent } of nodesToReplace.reverse()) {
-        const altText = figure.children[0].alt;
-        const url = figure.children[0].url;
-
-        // Convert the AST of the paragraph children to HTML
-        const rehypeAst = unified().use(remark2rehype).runSync(caption.children[0]);
-        const figcaptionHtml = unified()
-            .use(html)
-            .stringify(rehypeAst)
-            .replace(/^<p>/, "")
-            .replace(/<\/p>$/, "");
-
-        parent.splice(index, 2, {
-            type: "html",
-            value: `<figure>\n  <img src="${url}" alt="${altText}"/>\n  <figcaption>${figcaptionHtml}</figcaption>\n</figure>`,
-        });
-    }
-};
 
 export const ArticleContent = ({ article }: Properties): JSX.Element => (
     <div className="article-content">
         <Markdown
             rehypePlugins={[rehypeRaw]}
-            remarkPlugins={[remarkGfm, remarkFigure]}
+            remarkPlugins={[remarkFigurePlugin]}
         >
             {article.content}
         </Markdown>
---
+++ resources/js/Utils/Remark/remarkFigurePlugin.ts
@@ -0,0 +1,63 @@
+import html from "rehype-stringify";
+import remark2rehype from "remark-rehype";
+import { unified } from "unified";
+import { type Node } from "unist";
+
+interface NodeWithChildren extends Node {
+    children: NodeWithChildren[];
+    alt?: string;
+    url?: string;
+}
+
+interface NodeOptionalChildren extends Omit<Node, "children"> {
+    children?: NodeWithChildren[];
+    value?: string;
+}
+
+export const remarkFigurePlugin = () => (tree: NodeWithChildren) => {
+    const nodesToReplace: Array<{
+        figure: NodeWithChildren;
+        caption: NodeWithChildren;
+        parent: NodeOptionalChildren[];
+        index: number;
+    }> = [];
+
+    for (let index = 0; index < tree.children.length; index++) {
+        const node: NodeWithChildren = tree.children[index];
+
+        if (node.type === "blockquote") {
+            const previous: NodeWithChildren = tree.children[index - 1];
+
+            if (
+                previous.type === "paragraph" &&
+                "children" in previous &&
+                Array.isArray(previous.children) &&
+                previous.children.length > 0 &&
+                previous.children[0]?.type === "image"
+            ) {
+                nodesToReplace.push({ figure: previous, caption: node, index: index - 1, parent: tree.children });
+            }
+        }
+    }
+
+    for (const { figure, caption, index, parent } of nodesToReplace.reverse()) {
+        const altText = figure.children[0].alt;
+        const url = figure.children[0].url;
+
+        const rehypeAst = unified()
+            .use(remark2rehype)
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
+            .runSync(caption.children[0] as any);
+
+        const figcaptionHtml = unified()
+            .use(html)
+            .stringify(rehypeAst)
+            .replace(/^<p>/, "")
+            .replace(/<\/p>$/, "");
+
+        parent.splice(index, 2, {
+            type: "html",
+            value: `<figure>\n  <img src="${url}" alt="${altText}"/>\n  <figcaption>${figcaptionHtml}</figcaption>\n</figure>`,
+        });
+    }
+};
