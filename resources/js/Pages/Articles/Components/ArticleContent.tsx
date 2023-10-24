d36f422bee84a2c9d69b91fd5316cfe0579c519b
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-24 13:26:10 -0600

    open linsk external

--- resources/js/Pages/Articles/Components/ArticleContent.tsx
+++ resources/js/Pages/Articles/Components/ArticleContent.tsx
@@ -1,7 +1,8 @@
 import Markdown from "react-markdown";
+import rehypeExternalLinks from "rehype-external-links";
 import rehypeRaw from "rehype-raw";
-
 import { remarkFigurePlugin } from "@/Utils/Remark/remarkFigurePlugin";
+
 interface Properties {
     article: App.Data.Articles.ArticleData;
 }
@@ -9,8 +10,9 @@ interface Properties {
 export const ArticleContent = ({ article }: Properties): JSX.Element => (
     <div className="article-content">
         <Markdown
-            rehypePlugins={[rehypeRaw]}
+            rehypePlugins={[rehypeRaw, [rehypeExternalLinks, { target: "_blank" }]]}
             remarkPlugins={[remarkFigurePlugin]}
+            skipHtml
         >
             {article.content}
         </Markdown>
