b599f9716adad7a4f7d89869e6b213d7afcc7308
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-24 13:30:53 -0600

    Update remarkFigurePlugin.ts

--- resources/js/Utils/Remark/remarkFigurePlugin.ts
+++ resources/js/Utils/Remark/remarkFigurePlugin.ts
@@ -14,52 +14,52 @@ interface NodeOptionalChildren extends Omit<Node, "children"> {
     value?: string;
 }
 
-export const remarkFigurePlugin =
-    () =>
-    (tree: NodeWithChildren): void => {
-        const nodesToReplace: Array<{
-            figure: NodeWithChildren;
-            caption: NodeWithChildren;
-            parent: NodeOptionalChildren[];
-            index: number;
-        }> = [];
+const transformTree = (tree: NodeWithChildren): void => {
+    const nodesToReplace: Array<{
+        figure: NodeWithChildren;
+        caption: NodeWithChildren;
+        parent: NodeOptionalChildren[];
+        index: number;
+    }> = [];
 
-        for (let index = 0; index < tree.children.length; index++) {
-            const node: NodeWithChildren = tree.children[index];
+    for (let index = 0; index < tree.children.length; index++) {
+        const node: NodeWithChildren = tree.children[index];
 
-            if (node.type === "blockquote") {
-                const previous: NodeWithChildren = tree.children[index - 1];
+        if (node.type === "blockquote") {
+            const previous: NodeWithChildren = tree.children[index - 1];
 
-                if (
-                    previous.type === "paragraph" &&
-                    "children" in previous &&
-                    Array.isArray(previous.children) &&
-                    previous.children.length > 0 &&
-                    previous.children[0]?.type === "image"
-                ) {
-                    nodesToReplace.push({ figure: previous, caption: node, index: index - 1, parent: tree.children });
-                }
+            if (
+                previous.type === "paragraph" &&
+                "children" in previous &&
+                Array.isArray(previous.children) &&
+                previous.children.length > 0 &&
+                previous.children[0]?.type === "image"
+            ) {
+                nodesToReplace.push({ figure: previous, caption: node, index: index - 1, parent: tree.children });
             }
         }
+    }
 
-        for (const { figure, caption, index, parent } of nodesToReplace.reverse()) {
-            const altText = figure.children[0].alt;
-            const url = figure.children[0].url;
+    for (const { figure, caption, index, parent } of nodesToReplace.reverse()) {
+        const altText = figure.children[0].alt;
+        const url = figure.children[0].url;
 
-            const rehypeAst = unified()
-                .use(remark2rehype)
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
-                .runSync(caption.children[0] as any);
+        const rehypeAst = unified()
+            .use(remark2rehype)
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-explicit-any
+            .runSync(caption.children[0] as any);
 
-            const figcaptionHtml = unified()
-                .use(html)
-                .stringify(rehypeAst)
-                .replace(/^<p>/, "")
-                .replace(/<\/p>$/, "");
+        const figcaptionHtml = unified()
+            .use(html)
+            .stringify(rehypeAst)
+            .replace(/^<p>/, "")
+            .replace(/<\/p>$/, "");
 
-            parent.splice(index, 2, {
-                type: "html",
-                value: `<figure>\n  <img src="${url}" alt="${altText}"/>\n  <figcaption>${figcaptionHtml}</figcaption>\n</figure>`,
-            });
-        }
-    };
+        parent.splice(index, 2, {
+            type: "html",
+            value: `<figure>\n  <img src="${url}" alt="${altText}"/>\n  <figcaption>${figcaptionHtml}</figcaption>\n</figure>`,
+        });
+    }
+};
+
+export const remarkFigurePlugin = (): ((tree: NodeWithChildren) => void) => transformTree;
