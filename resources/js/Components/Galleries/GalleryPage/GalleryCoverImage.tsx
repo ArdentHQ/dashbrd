dde50f3d8f511e8f9719cdad19ae7d5c6ef12eaf
goga-m <goga.mamoulasvili@gmail.com>
2023-10-26 12:42:17 +0200

    wip

--- resources/js/Components/Galleries/GalleryPage/GalleryCoverImage.tsx
+++ resources/js/Components/Galleries/GalleryPage/GalleryCoverImage.tsx
@@ -1,13 +1,10 @@
 import { Img } from "@/Components/Image";
 
 export const GalleryCoverImage = ({ image }: { image: string }): JSX.Element => (
-    <div
-        className="mb-3 aspect-[3/2]"
+    <Img
         data-testid="GalleryCoverImage"
-    >
-        <Img
-            src={image}
-            className="aspect-[3/2] min-w-full rounded-xl object-cover"
-        />
-    </div>
+        wrapperClassName="mb-3 aspect-[3/2]"
+        src={image}
+        className="rounded-xl"
+    />
 );
