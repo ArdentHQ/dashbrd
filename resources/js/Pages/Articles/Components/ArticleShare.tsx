3e169b20e11aac64f7cb40d5ae61949ec98b9661
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-10-26 10:17:08 -0500

    feat: dark mode for article share component

--- resources/js/Pages/Articles/Components/ArticleShare.tsx
+++ resources/js/Pages/Articles/Components/ArticleShare.tsx
@@ -1,11 +1,30 @@
+import { useTranslation } from "react-i18next";
 import { ButtonLink } from "@/Components/Buttons/ButtonLink";
+import { Tooltip } from "@/Components/Tooltip";
 
 interface Properties extends React.AnchorHTMLAttributes<HTMLUListElement> {
     article: App.Data.Articles.ArticleData;
 }
 
+const ShareTooltip = ({ platform, children }: { platform: string; children: JSX.Element }): JSX.Element => {
+    const { t } = useTranslation();
+
+    return (
+        <Tooltip
+            content={t("common.share_on", {
+                platform,
+            })}
+            placement="left"
+        >
+            {children}
+        </Tooltip>
+    );
+};
+
 export const ArticleShare = ({ article, ...properties }: Properties): JSX.Element => {
     const url = route("articles.view", { article: article.slug });
+
+    const { t } = useTranslation();
 
     const titleUrlQuery = new URLSearchParams({ title: article.title, url }).toString();
 
@@ -14,28 +33,34 @@ export const ArticleShare = ({ article, ...properties }: Properties): JSX.Elemen
     return (
         <ul {...properties}>
             <li>
-                <ButtonLink
-                    variant="icon"
-                    href={`https://twitter.com/intent/tweet?${titleUrlQuery}`}
-                    icon="Twitter"
-                    target="_blank"
-                />
+                <ShareTooltip platform={t("common.x")}>
+                    <ButtonLink
+                        variant="icon"
+                        href={`https://twitter.com/intent/tweet?${titleUrlQuery}`}
+                        icon="Twitter"
+                        target="_blank"
+                    />
+                </ShareTooltip>
             </li>
             <li>
-                <ButtonLink
-                    variant="icon"
-                    href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}
-                    icon="Facebook"
-                    target="_blank"
-                />
+                <ShareTooltip platform={t("common.facebook")}>
+                    <ButtonLink
+                        variant="icon"
+                        href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}
+                        icon="Facebook"
+                        target="_blank"
+                    />
+                </ShareTooltip>
             </li>
             <li>
-                <ButtonLink
-                    variant="icon"
-                    href={`https://www.reddit.com/submit?${urlQuery}`}
-                    icon="Reddit"
-                    target="_blank"
-                />
+                <ShareTooltip platform={t("common.reddit")}>
+                    <ButtonLink
+                        variant="icon"
+                        href={`https://www.reddit.com/submit?${urlQuery}`}
+                        icon="Reddit"
+                        target="_blank"
+                    />
+                </ShareTooltip>
             </li>
         </ul>
     );
