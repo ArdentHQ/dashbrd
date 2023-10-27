c56059b2b008eb453e40061330ad352d60dc7748
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-27 08:59:46 -0600

    add space for the outline

--- resources/js/Pages/Collections/Components/Articles/ArticlesScroll.tsx
+++ resources/js/Pages/Collections/Components/Articles/ArticlesScroll.tsx
@@ -14,7 +14,7 @@ export const ArticlesScroll = ({ articles }: { articles: App.Data.Articles.Artic
             <Carousel
                 horizontalOffset={horizontalOffset}
                 headerClassName="mx-6 sm:mx-8 2xl:mx-0"
-                swiperClassName="-m-1 lg:mx-7 2xl:-m-1"
+                swiperClassName="m-[-3px] lg:mx-[29px] lg:my-[-3px] 2xl:m-[-3px]"
                 spaceBetween={8}
                 slidesPerView={slidesPerView}
                 title={
@@ -32,10 +32,9 @@ export const ArticlesScroll = ({ articles }: { articles: App.Data.Articles.Artic
             >
                 {articles.map((article) => (
                     <CarouselItem key={article.id}>
-                        <ArticleCard
-                            className="m-1"
-                            article={article}
-                        />
+                        <div className="h-full p-[3px]">
+                            <ArticleCard article={article} />
+                        </div>
                     </CarouselItem>
                 ))}
             </Carousel>
