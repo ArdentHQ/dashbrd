b81f0b00dd65c7d1e50cfd6bbd0de508cfe1526f
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-10-30 04:29:15 -0600

    refactor: ignore markdown on metadata (#287)

--- app/Observers/ArticleObserver.php
+++ app/Observers/ArticleObserver.php
@@ -6,17 +6,22 @@ namespace App\Observers;
 
 use App\Jobs\ConvertArticleToSpeech;
 use App\Models\Article;
+use Illuminate\Support\Facades\Cache;
 
 class ArticleObserver
 {
     public function created(Article $article): void
     {
         $this->prepareArticleSpeech($article);
+
+        $this->forgetMetaDescriptionCache($article);
     }
 
     public function updated(Article $article): void
     {
         $this->prepareArticleSpeech($article);
+
+        $this->forgetMetaDescriptionCache($article);
     }
 
     private function prepareArticleSpeech(Article $article): void
@@ -38,5 +43,18 @@ class ArticleObserver
         }
 
         ConvertArticleToSpeech::dispatch($article);
+    }
+
+    private function forgetMetaDescriptionCache(Article $article): void
+    {
+        if ($article->isNotPublished()) {
+            return;
+        }
+
+        if (! ($article->wasRecentlyCreated || $article->wasChanged(['meta_description', 'content', 'is_published']))) {
+            return;
+        }
+
+        Cache::forget("article:{$article->id}:meta_description");
     }
 }
--- tests/App/Observers/ArticleObserverTest.php
+++ tests/App/Observers/ArticleObserverTest.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 use App\Jobs\ConvertArticleToSpeech;
 use App\Models\Article;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Cache;
 
 beforeEach(function () {
     config(['dashbrd.text_to_speech.enabled' => true]);
+
+    Article::truncate();
 });
 
 it('should not start audio conversion if text-to-speech is not enabled', function () {
@@ -114,4 +117,76 @@ it('should not start preparing audio on creation if tts is disabled via job', fu
     Bus::assertNotDispatched(ConvertArticleToSpeech::class);
 
     ConvertArticleToSpeech::enable();
+});
+
+it('should not clear the meta description cache if article is not published', function () {
+    Cache::shouldReceive('forget')->never();
+
+    Article::factory()->create([
+        'content' => 'Hello World',
+        'meta_description' => 'Hello World',
+        'published_at' => null,
+    ]);
+});
+
+it('should not clear the meta description cache if article content or meta description did not change', function () {
+    $article = Article::factory()->create([
+        'content' => 'Hello World',
+        'meta_description' => 'Hello World',
+        'published_at' => now()->subMinutes(2),
+    ])->fresh();
+
+    Cache::shouldReceive('forget')->never();
+
+    $article->update([
+        'title' => 'Updated title',
+    ]);
+});
+
+it('should clear the meta description cache when an article created', function () {
+    Cache::shouldReceive('forget')->with('article:1:meta_description')->once();
+
+    Article::factory()->create([
+        'content' => 'Hello World',
+        'published_at' => now()->subMinutes(2),
+    ]);
+});
+
+it('should clear the meta description cache when an article content is updated', function () {
+    $article = Article::factory()->create([
+        'content' => 'Hello World',
+        'published_at' => now()->subMinutes(2),
+    ]);
+
+    Cache::shouldReceive('forget')->with('article:1:meta_description')->once();
+
+    $article->update([
+        'content' => 'Updated content',
+    ]);
+});
+
+it('should clear the meta description cache when an article meta_description is updated', function () {
+    $article = Article::factory()->create([
+        'content' => 'Hello World',
+        'published_at' => now()->subMinutes(2),
+    ]);
+
+    Cache::shouldReceive('forget')->with('article:1:meta_description')->once();
+
+    $article->update([
+        'meta_description' => 'Updated content',
+    ]);
+});
+
+it('should clear the meta description cache when an article is published', function () {
+    $article = Article::factory()->create([
+        'content' => 'Hello World',
+        'published_at' => null,
+    ]);
+
+    Cache::shouldReceive('forget')->with('article:1:meta_description')->once();
+
+    $article->update([
+        'published_at' => now()->subMinutes(2),
+    ]);
 });
