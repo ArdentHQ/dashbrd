a87f64bdd3e9544441701b9ea27462f77676846b
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-10-31 12:50:06 -0500

    test: NftGalleryCard

---
+++ resources/js/Components/Drafts/NftGalleryCard.test.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { NftDraftCard } from "./NftDraftCard";
+import * as useMetaMaskContext from "@/Contexts/MetaMaskContext";
+import * as useAuth from "@/Hooks/useAuth";
+import GalleryDataFactory from "@/Tests/Factories/Gallery/GalleryDataFactory";
+import UserDataFactory from "@/Tests/Factories/UserDataFactory";
+import { getSampleMetaMaskState } from "@/Tests/SampleData/SampleMetaMaskState";
+import { render, screen } from "@/Tests/testing-library";
+
+const user = new UserDataFactory().create();
+
+describe("NftGalleryCard", () => {
+    vi.spyOn(useAuth, "useAuth").mockReturnValue({
+        user,
+        wallet: null,
+        authenticated: true,
+        showAuthOverlay: false,
+        showCloseButton: false,
+        signed: false,
+        closeOverlay: vi.fn(),
+    });
+
+    vi.spyOn(useMetaMaskContext, "useMetaMaskContext").mockReturnValue(getSampleMetaMaskState());
+
+    it("shows an NFT gallery card for the user", () => {
+        const gallery = new GalleryDataFactory().withCoverImage().create();
+
+        render(<NftDraftCard gallery={gallery} />);
+
+        expect(screen.getByTestId("NftGalleryCard")).toBeInTheDocument();
+
+        expect(screen.getByTestId("GalleryCoverImage")).toBeInTheDocument();
+        expect(screen.queryByTestId("NftImageGrid")).not.toBeInTheDocument();
+    });
+
+    it("shows an NFT gallery card for the user when no cover image is set", () => {
+        const gallery = new GalleryDataFactory().withoutCoverImage().create();
+
+        render(<NftDraftCard gallery={gallery} />);
+
+        expect(screen.getByTestId("NftGalleryCard")).toBeInTheDocument();
+
+        expect(screen.getByTestId("NftImageGrid")).toBeInTheDocument();
+        expect(screen.queryByTestId("GalleryCoverImage")).not.toBeInTheDocument();
+    });
+});
