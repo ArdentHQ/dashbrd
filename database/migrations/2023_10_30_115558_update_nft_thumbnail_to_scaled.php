a2d79ade69ab462a854c48aa1be2e7d8fb609078
goga-m <goga.mamoulasvili@gmail.com>
2023-10-31 16:06:24 +0100

    wip

--- database/migrations/2023_10_30_115558_update_nft_thumbnail_to_scaled.php
+++ database/migrations/2023_10_30_115558_update_nft_thumbnail_to_scaled.php
@@ -5,32 +5,12 @@ declare(strict_types=1);
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-return new class() extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        DB::table('nfts')
-            ->whereRaw("extra_attributes->'images'->>'thumb' LIKE '%thumbnailv2%'")
-            ->orWhereRaw("extra_attributes->'images'->>'small' LIKE '%thumbnailv2%'")
-            ->orWhereRaw("extra_attributes->'images'->>'large' LIKE '%thumbnailv2%'")->get()->each(function ($nft) {
-                $extraAttributes = json_decode($nft->extra_attributes);
-
-                $extraAttributes->images->thumb = $this->replaceThumbnailsWithScaled($extraAttributes->images->thumb);
-                $extraAttributes->images->small = $this->replaceThumbnailsWithScaled($extraAttributes->images->small);
-                $extraAttributes->images->large = $this->replaceThumbnailsWithScaled($extraAttributes->images->large);
-
-                DB::table('nfts')
-                    ->where('id', $nft->id)
-                    ->update(['extra_attributes' => json_encode($extraAttributes)]);
-
-            });
-    }
-
-    public function replaceThumbnailsWithScaled(string $url): string
-    {
-        return preg_replace('/thumbnailv2/', 'scaled', $url);
+        DB::select(get_query('nfts.update_thumbnailv2_to_scaled'));
     }
 };
