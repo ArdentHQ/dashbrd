177eb72921ef18b32f8b877cb4c79be8d38d153c
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2023-10-31 10:43:05 +0100

    chore: update generated types file (#321)

--- resources/types/generated.d.ts
+++ resources/types/generated.d.ts
@@ -242,10 +242,6 @@ declare namespace App.Data.Gallery {
         isOwner: boolean;
         hasLiked: boolean;
     };
-    export type GalleryLikeData = {
-        likes: number;
-        hasLiked: boolean;
-    };
     export type GalleryNftData = {
         id: number;
         name: string | null;
