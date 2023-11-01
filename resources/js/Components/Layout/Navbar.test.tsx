667506730d722dd39b981d1f7c5edfd65ae72e5b
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2023-11-01 12:22:53 +0100

    fix test

--- resources/js/Components/Layout/Navbar.test.tsx
+++ resources/js/Components/Layout/Navbar.test.tsx
@@ -4,6 +4,7 @@ import UserDataFactory from "@/Tests/Factories/UserDataFactory";
 import WalletFactory from "@/Tests/Factories/Wallet/WalletFactory";
 import { useTransactionSliderContextSpy } from "@/Tests/Spies/useTransactionSliderContextSpy";
 import { mockAuthContext, render, screen, userEvent } from "@/Tests/testing-library";
+import DarkModeContextProvider from "@/Contexts/DarkModeContext";
 const user = new UserDataFactory().create();
 
 const wallet = new WalletFactory().create();
@@ -15,15 +16,17 @@ describe("Navbar", () => {
         const resetMock = mockAuthContext({ user, wallet });
 
         render(
-            <Navbar
-                authenticated
-                connecting
-                initialized
-                user={user}
-                wallet={wallet}
-                connectWallet={vi.fn()}
-                onLogout={vi.fn()}
-            />,
+            <DarkModeContextProvider>
+                <Navbar
+                    authenticated
+                    connecting
+                    initialized
+                    user={user}
+                    wallet={wallet}
+                    connectWallet={vi.fn()}
+                    onLogout={vi.fn()}
+                />
+            </DarkModeContextProvider>,
         );
 
         expect(screen.getByTestId("Navbar")).toBeInTheDocument();
@@ -35,14 +38,16 @@ describe("Navbar", () => {
         const connectMock = vi.fn();
 
         render(
-            <Navbar
-                authenticated={false}
-                connecting={false}
-                initialized
-                wallet={null}
-                connectWallet={connectMock}
-                onLogout={vi.fn()}
-            />,
+            <DarkModeContextProvider>
+                <Navbar
+                    authenticated={false}
+                    connecting={false}
+                    initialized
+                    wallet={null}
+                    connectWallet={connectMock}
+                    onLogout={vi.fn()}
+                />
+            </DarkModeContextProvider>,
         );
 
         expect(screen.getByTestId("Navbar__connect")).toBeInTheDocument();
