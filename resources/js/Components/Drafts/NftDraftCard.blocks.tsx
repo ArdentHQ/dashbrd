9b54db1d591a0ccfa781171c2817d43f580b6e59
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-11-02 08:11:36 -0500

    refactor: trash icon for nft draft card footer

--- resources/js/Components/Drafts/NftDraftCard.blocks.tsx
+++ resources/js/Components/Drafts/NftDraftCard.blocks.tsx
@@ -1,12 +1,14 @@
 import { useTranslation } from "react-i18next";
+import { IconButton } from "@/Components/Buttons/IconButton";
 import { Icon } from "@/Components/Icon";
+import { Tooltip } from "@/Components/Tooltip";
 
 export const NftDraftFooter = (): JSX.Element => {
     const { t } = useTranslation();
 
     return (
         <div
-            className="flex items-center justify-between text-theme-secondary-700 dark:text-theme-dark-200"
+            className="-my-1.5 flex items-center justify-between text-theme-secondary-700 dark:text-theme-dark-200"
             data-testid="NftDraftCard__Footer"
         >
             <div className="flex items-center space-x-2">
@@ -18,11 +20,14 @@ export const NftDraftFooter = (): JSX.Element => {
                 <span className="text-sm text-theme-secondary-700 dark:text-theme-dark-200">{t("common.draft")}</span>
             </div>
             <div className="flex items-center space-x-2">
-                <Icon
-                    name="Trash"
-                    size="lg"
-                    className="text-theme-primary-900 dark:text-theme-dark-200"
-                />
+                <Tooltip content={t("common.delete_draft")}>
+                    <IconButton
+                        icon="Trash"
+                        iconClass="text-theme-primary-900 dark:text-theme-dark-200"
+                        iconSize="md"
+                        className="border-transparent bg-transparent dark:border-transparent"
+                    />
+                </Tooltip>
             </div>
         </div>
     );
