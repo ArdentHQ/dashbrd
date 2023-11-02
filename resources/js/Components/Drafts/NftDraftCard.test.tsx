8c5ea223d91968b1d01519124a03e9b756614388
ItsANameToo <ItsANameToo@users.noreply.github.com>
2023-11-02 15:39:03 +0000

    style: resolve linting violations

--- resources/js/Components/Drafts/NftDraftCard.test.tsx
+++ resources/js/Components/Drafts/NftDraftCard.test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
+import { type SpyInstance } from "vitest";
 import { NftDraftCard } from "./NftDraftCard";
 import * as useMetaMaskContext from "@/Contexts/MetaMaskContext";
 import GalleryDataFactory from "@/Tests/Factories/Gallery/GalleryDataFactory";
 import UserDataFactory from "@/Tests/Factories/UserDataFactory";
 import { getSampleMetaMaskState } from "@/Tests/SampleData/SampleMetaMaskState";
 import { mockAuthContext, render, screen } from "@/Tests/testing-library";
-import { SpyInstance } from "vitest";
 
 const user = new UserDataFactory().create();
 let resetAuthContextMock: () => void;
