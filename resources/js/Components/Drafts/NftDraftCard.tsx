70a413569b2a822b7110cec29301ab4a04f8dd37
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-11-03 15:10:34 -0500

    refactor: NftDraftCard

--- resources/js/Components/Drafts/NftDraftCard.tsx
+++ resources/js/Components/Drafts/NftDraftCard.tsx
@@ -1,38 +1,46 @@
 import { Link } from "@inertiajs/react";
 import cn from "classnames";
-import { NftDraftFooter } from "./NftDraftCard.blocks";
+import { NftDraftHeading, NftDraftImageGrid, NftDraftStats } from "./NftDraftCard.blocks";
 import { GalleryCoverImage } from "@/Components/Galleries/GalleryPage/GalleryCoverImage";
-import { GalleryHeading, GalleryStats, NftImageGrid } from "@/Components/Galleries/NftGalleryCard.blocks";
+import { GalleryDraft } from "@/Pages/Galleries/hooks/useGalleryDrafts";
+import { isTruthy } from "@/Utils/is-truthy";
 
-export const NftDraftCard = ({ gallery }: { gallery: App.Data.Gallery.GalleryData }): JSX.Element => (
-    <Link
-        href={route("galleries.view", gallery.slug)}
-        className="group focus-visible:outline-none focus-visible:ring-0"
-    >
-        <div
-            className={cn(
-                "transition-default m-1 box-content flex flex-col rounded-xl border border-theme-secondary-300 dark:border-theme-dark-700",
-                "outline outline-3 outline-transparent",
-                "group-hover:outline-theme-primary-100 dark:group-hover:outline-theme-dark-500",
-                "group-focus-visible:outline-theme-primary-300",
-            )}
-            data-testid="NftDraftCard"
+export const NftDraftCard = ({ draft }: { draft: GalleryDraft}): JSX.Element => {
+    let coverImage: string | null = null;
+
+    if (isTruthy(draft.cover) && isTruthy(draft.coverType)) {
+        coverImage = URL.createObjectURL(new Blob([draft.cover], { type: draft.coverType }));
+    }
+
+    return (
+        <Link
+            href={"#"}
+            className="group focus-visible:outline-none focus-visible:ring-0"
         >
-            <div className="px-6 pb-3 pt-6">
-                {gallery.coverImage !== null && <GalleryCoverImage image={gallery.coverImage} />}
+            <div
+                className={cn(
+                    "transition-default m-1 box-content flex flex-col rounded-xl border border-theme-secondary-300 dark:border-theme-dark-700",
+                    "outline outline-3 outline-transparent",
+                    "group-hover:outline-theme-primary-100 dark:group-hover:outline-theme-dark-500",
+                    "group-focus-visible:outline-theme-primary-300",
+                )}
+                data-testid="NftDraftCard"
+            >
+                <div className="px-6 pb-3 pt-6">
+                    {coverImage !== null && <GalleryCoverImage image={coverImage} />}
 
-                {gallery.coverImage === null && <NftImageGrid nfts={gallery.nfts} />}
+                    {draft.cover === null && <NftDraftImageGrid nfts={draft.nfts} />}
 
-                <GalleryHeading
-                    name={gallery.name}
-                    wallet={gallery.wallet}
+                    <NftDraftHeading
+                        title={draft.title}
+                        walletAddress={draft.walletAddress ?? ""}
+                    />
+                </div>
+
+                <NftDraftStats
+                    draft={draft}
                 />
             </div>
-
-            <GalleryStats
-                gallery={gallery}
-                footer={<NftDraftFooter />}
-            />
-        </div>
-    </Link>
-);
+        </Link>
+    )
+};
