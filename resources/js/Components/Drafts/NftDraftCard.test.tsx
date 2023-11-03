3d2a3aafed011cf0e2ea7c1a75bf966fa755a59f
patricio0312rev <patricio0312rev@users.noreply.github.com>
2023-11-03 20:40:23 +0000

    style: resolve style guide violations

--- resources/js/Components/Drafts/NftDraftCard.test.tsx
+++ resources/js/Components/Drafts/NftDraftCard.test.tsx
@@ -77,8 +77,6 @@ describe("NftDraftCard", () => {
 
         render(<NftDraftCard draft={draftWithoutWalletAddress} />);
 
-        expect(screen.getByTestId("GalleryHeading__address")).toHaveTextContent(
-            "0x000…0000",
-        );
+        expect(screen.getByTestId("GalleryHeading__address")).toHaveTextContent("0x000…0000");
     });
 });
