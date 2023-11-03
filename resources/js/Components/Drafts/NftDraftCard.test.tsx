cb6d3ff70ba4e18c113bc49333f5d5293d87145c
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-11-03 15:12:30 -0500

    test: NftDraftCard

--- resources/js/Components/Drafts/NftDraftCard.test.tsx
+++ resources/js/Components/Drafts/NftDraftCard.test.tsx
@@ -2,16 +2,27 @@ import React from "react";
 import { type SpyInstance } from "vitest";
 import { NftDraftCard } from "./NftDraftCard";
 import * as useMetaMaskContext from "@/Contexts/MetaMaskContext";
-import GalleryDataFactory from "@/Tests/Factories/Gallery/GalleryDataFactory";
 import UserDataFactory from "@/Tests/Factories/UserDataFactory";
 import { getSampleMetaMaskState } from "@/Tests/SampleData/SampleMetaMaskState";
 import { mockAuthContext, render, screen } from "@/Tests/testing-library";
+import { GalleryDraft } from "@/Pages/Galleries/hooks/useGalleryDrafts";
 
 const user = new UserDataFactory().create();
 let resetAuthContextMock: () => void;
 let useMetaMaskContextSpy: SpyInstance;
 
 describe("NftDraftCard", () => {
+    const draft: GalleryDraft = {
+        id: 1,
+        title: "Test draft",
+        cover: null,
+        coverType: null,
+        walletAddress: "0x22Fd644149ea87ca26237183ad6A66f91dfcFB87",
+        nfts: [],
+        value: "0",
+        collectionsCount: 0
+    }
+
     beforeEach(() => {
         resetAuthContextMock = mockAuthContext({ user });
 
@@ -26,25 +37,36 @@ describe("NftDraftCard", () => {
         useMetaMaskContextSpy.mockRestore();
     });
 
-    it("shows an NFT gallery card for the user", () => {
-        const gallery = new GalleryDataFactory().withCoverImage().create();
-
-        render(<NftDraftCard gallery={gallery} />);
+    it("renders", () => {
+        render(<NftDraftCard draft={draft} />);
 
         expect(screen.getByTestId("NftDraftCard")).toBeInTheDocument();
+    })
 
-        expect(screen.getByTestId("GalleryCoverImage")).toBeInTheDocument();
-        expect(screen.queryByTestId("NftImageGrid")).not.toBeInTheDocument();
-    });
 
     it("shows an NFT gallery card for the user when no cover image is set", () => {
-        const gallery = new GalleryDataFactory().withoutCoverImage().create();
+        render(<NftDraftCard draft={draft} />);
 
-        render(<NftDraftCard gallery={gallery} />);
-
-        expect(screen.getByTestId("NftDraftCard")).toBeInTheDocument();
-
-        expect(screen.getByTestId("NftImageGrid")).toBeInTheDocument();
         expect(screen.queryByTestId("GalleryCoverImage")).not.toBeInTheDocument();
+        expect(screen.getByTestId("NftDraftImageGrid")).toBeInTheDocument();
+    });
+
+    it("shows an NFT gallery card for the user with cover", () => {
+        const createObjectURL = vi.fn((blob) => `mocked-url:${blob.type}`);
+        const originalURL = globalThis.URL;
+        originalURL.createObjectURL = createObjectURL;
+
+        const bufferSize = 10;
+        const arrayBuffer = new ArrayBuffer(bufferSize);
+        const draftWithCover = {
+            ...draft,
+            cover: arrayBuffer,
+            coverType: "image/jpeg"
+        };
+
+        render(<NftDraftCard draft={draftWithCover} />);
+
+        expect(screen.getByTestId("GalleryCoverImage")).toBeInTheDocument();
+        expect(screen.queryByTestId("NftDraftImageGrid")).not.toBeInTheDocument();
     });
 });
