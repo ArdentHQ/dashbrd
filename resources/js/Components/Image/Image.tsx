50821fb0c83681bc44ec949644397616466086f3
George Mamoulasvili <goga.mamoulasvili@gmail.com>
2023-11-07 16:51:25 +0100

    fix: correct size for errored image placeholder (#386)

--- resources/js/Components/Image/Image.tsx
+++ resources/js/Components/Image/Image.tsx
@@ -80,7 +80,7 @@ export const Img = ({
                 {isErrored && (
                     <ImageErrorPlaceholder
                         errorMessage={errorMessage}
-                        className={cn(className, errorClassName, "aspect-square")}
+                        className={cn(className, errorClassName, "h-full w-full")}
                     />
                 )}
             </div>
