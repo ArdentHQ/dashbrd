90809ca687a674c765455d4ae485024d0ac324dd
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2023-11-08 13:22:09 +0100

    Update DeleteGalleryButton.test.tsx

--- resources/js/Components/Galleries/DeleteGalleryButton.test.tsx
+++ resources/js/Components/Galleries/DeleteGalleryButton.test.tsx
@@ -22,8 +22,24 @@ describe("DeleteGalleryButton", () => {
         expect(screen.getByTestId("ConfirmationDialog__form")).toBeInTheDocument();
     });
 
+    it("closes the slideout panel when close button is pressed", async () => {
+        render(<DeleteGalleryButton gallery={gallery} />);
+
+        expect(screen.queryByTestId("ConfirmationDialog__form")).not.toBeInTheDocument();
+
+        await userEvent.click(screen.getByTestId("DeleteGalleryButton"));
+
+        expect(screen.getByTestId("ConfirmationDialog__form")).toBeInTheDocument();
+
+        await userEvent.click(screen.getByTestId("ConfirmationDialog__close"));
+
+        expect(screen.queryByTestId("ConfirmationDialog__form")).not.toBeInTheDocument();
+    });
+
     it("deletes the gallery when submitted", async () => {
-        const submitFunction = vi.fn();
+        const submitFunction = vi.fn().mockImplementation((_, options) => {
+            options?.onFinish?.();
+        });
 
         mockInertiaUseForm({
             data: {},
@@ -39,5 +55,7 @@ describe("DeleteGalleryButton", () => {
         await userEvent.click(screen.getByTestId("ConfirmationDialog__confirm"));
 
         expect(submitFunction).toHaveBeenCalled();
+
+        expect(screen.queryByTestId("ConfirmationDialog__form")).not.toBeInTheDocument();
     });
 });
