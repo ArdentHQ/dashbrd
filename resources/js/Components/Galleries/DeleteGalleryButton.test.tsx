fed453b1e951bedf2c30275ad824f0db5ee55358
crnkovic <crnkovic@users.noreply.github.com>
2023-11-08 12:03:44 +0000

    style: resolve style guide violations

--- resources/js/Components/Galleries/DeleteGalleryButton.test.tsx
+++ resources/js/Components/Galleries/DeleteGalleryButton.test.tsx
@@ -1,7 +1,7 @@
 import DeleteGalleryButton from "./DeleteGalleryButton";
 import GalleryDataFactory from "@/Tests/Factories/Gallery/GalleryDataFactory";
 import { mockInertiaUseForm, render, screen } from "@/Tests/testing-library";
-import userEvent from "@testing-library/user-event"
+import userEvent from "@testing-library/user-event";
 
 describe("DeleteGalleryButton", () => {
     const gallery = new GalleryDataFactory().create();
