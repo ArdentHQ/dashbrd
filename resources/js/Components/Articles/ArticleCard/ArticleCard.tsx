517ca9628a38060e549bdb1a3de9350e484f11bd
goga-m <goga.mamoulasvili@gmail.com>
2023-11-09 13:16:55 +0100

    wip

--- resources/js/Components/Articles/ArticleCard/ArticleCard.tsx
+++ resources/js/Components/Articles/ArticleCard/ArticleCard.tsx
@@ -51,7 +51,7 @@ export const ArticleCard = ({
                     alt={article.title}
                     srcSet={`${article.image.medium} 1x, ${article.image.medium2x} 2x`}
                     src={article.image.medium}
-                    errorPlaceholder={isLargeVariant ? <ArticleErrorImage /> : undefined}
+                    errorPlaceholder={isLargeVariant ? <ArticleErrorImage isLargeVariant /> : undefined}
                 />
             </div>
 
--- resources/js/Components/Articles/ArticleCard/ArticleErrorImage.tsx
+++ resources/js/Components/Articles/ArticleCard/ArticleErrorImage.tsx
@@ -1,14 +1,40 @@
 import cn from "classnames";
-import { ImageLoadError, ImageLoadErrorPrimary } from "@/images";
+import { ImageLoadError, ImageLoadErrorDark, ImageLoadErrorPrimary } from "@/images";
+import { isTruthy } from "@/Utils/is-truthy";
 
-export const ArticleErrorImage = ({ className }: { className?: string; errorMessage?: string }): JSX.Element => (
-    <div
-        data-testid="ArticleErrorImage"
-        className={cn("h-full w-full bg-theme-secondary-100 p-4 dark:bg-theme-primary-800", className)}
-    >
-        <div className="flex h-full flex-col items-center justify-center">
-            <ImageLoadError className="mx-auto max-w-23 dark:hidden" />
-            <ImageLoadErrorPrimary className="mx-auto hidden max-w-23 dark:block" />
+export const ArticleErrorImage = ({
+    className,
+    isLargeVariant,
+}: {
+    className?: string;
+    isLargeVariant?: boolean;
+}): JSX.Element => {
+    if (isTruthy(isLargeVariant)) {
+        return (
+            <div
+                data-testid="ArticleErrorImage"
+                className={cn(
+                    "h-full w-full bg-theme-dark-800 p-4 hover:bg-theme-primary-800 dark:bg-theme-primary-800",
+                    className,
+                )}
+            >
+                <div className="flex h-full flex-col items-center justify-center">
+                    <ImageLoadErrorDark className="mx-auto max-w-23 group-hover:hidden dark:hidden" />
+                    <ImageLoadErrorPrimary className="mx-auto hidden max-w-23 group-hover:block dark:block" />
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div
+            data-testid="ArticleErrorImage"
+            className={cn("h-full w-full bg-theme-secondary-100 p-4 dark:bg-theme-primary-800", className)}
+        >
+            <div className="flex h-full flex-col items-center justify-center">
+                <ImageLoadError className="mx-auto max-w-23 dark:hidden" />
+                <ImageLoadErrorPrimary className="mx-auto hidden max-w-23 dark:block" />
+            </div>
         </div>
-    </div>
-);
+    );
+};
