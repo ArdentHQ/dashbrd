d139a845e3926339616f6ff7d32ca01bb044857a
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-11-09 16:25:22 -0500

    fix: use LogoutController instead of route name

--- tests/App/Http/Controllers/Filament/LogoutControllerTest.php
+++ tests/App/Http/Controllers/Filament/LogoutControllerTest.php
@@ -3,10 +3,11 @@
 declare(strict_types=1);
 
 use App\Models\User;
+use App\Http\Controllers\Filament\LogoutController;
 
 it('should redirect the admin user to homepage on logout', function () {
     $user = User::factory()->editor()->create();
 
-    $this->actingAs($user)->post(route('filament.admin.auth.logout'))
+    $this->actingAs($user)->post(action([LogoutController::class, 'logout']))
         ->assertRedirect(route('galleries'));
 });
