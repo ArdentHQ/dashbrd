0bdf91b768efb453cba3e30ea6926022400b7bf5
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-11-09 09:00:01 -0500

    feat: custom filament logout controller

---
+++ app/Http/Controllers/Filament/LogoutController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Filament;
+
+use Filament\Facades\Filament;
+use Filament\Http\Controllers\Auth\LogoutController as FilamentLogoutController;
+use Illuminate\Http\Request;
+
+class LogoutController extends FilamentLogoutController
+{
+    public function logout(Request $request)
+    {
+        Filament::auth()->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('galleries');
+    }
+}
