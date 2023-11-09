92b81abb1f18aa413e5047c95de5a5c33e217c52
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-11-09 09:52:29 -0500

    test: LogoutControllerTest

---
+++ tests/App/Http/Controllers/Filament/LogoutControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+it('should redirect the admin user to homepage on logout', function () {
+    $user = User::factory()->create();
+
+    # insert into model_has_roles (role_id, model_type, model_id) values (1, 'App\\Models\\User', 1); in the following line
+    DB::insert(
+        'insert into model_has_roles (role_id, model_type, model_id) values (?, ?, ?)',
+        [1, 'App\\Models\\User', $user->id]
+    );
+
+    $this->post(route('filament.admin.auth.logout'))
+        ->assertRedirect(route('galleries'));
+});
