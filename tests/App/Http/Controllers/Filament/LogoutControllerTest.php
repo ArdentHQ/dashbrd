7576e81e0cc6232da8ea838b542728876d2871cd
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-11-09 10:20:35 -0500

    refactor: set user as editor for logout controller test

--- tests/App/Http/Controllers/Filament/LogoutControllerTest.php
+++ tests/App/Http/Controllers/Filament/LogoutControllerTest.php
@@ -3,16 +3,10 @@
 declare(strict_types=1);
 
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
 
 it('should redirect the admin user to homepage on logout', function () {
-    $user = User::factory()->create();
+    $user = User::factory()->editor()->create();
 
-    DB::insert(
-        'insert into model_has_roles (role_id, model_type, model_id) values (?, ?, ?)',
-        [1, 'App\\Models\\User', $user->id]
-    );
-
-    $this->post(route('filament.admin.auth.logout'))
+    $this->actingAs($user)->post(route('filament.admin.auth.logout'))
         ->assertRedirect(route('galleries'));
 });
