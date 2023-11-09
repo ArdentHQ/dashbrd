d3a15d80dd14536fde547079a7e928966d8d4d6c
patricio0312rev <patricio0312rev@users.noreply.github.com>
2023-11-09 21:26:52 +0000

    style: resolve style guide violations

--- tests/App/Http/Controllers/Filament/LogoutControllerTest.php
+++ tests/App/Http/Controllers/Filament/LogoutControllerTest.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Models\User;
 use App\Http\Controllers\Filament\LogoutController;
+use App\Models\User;
 
 it('should redirect the admin user to homepage on logout', function () {
     $user = User::factory()->editor()->create();
