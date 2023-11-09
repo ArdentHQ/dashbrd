106c93242bbb67995dc4c0d9df44ccb18112b584
goga-m <goga.mamoulasvili@gmail.com>
2023-11-09 11:21:36 +0100

    wip

--- resources/js/Components/Articles/ArticleCard/ArticleErrorImage.tsx
+++ resources/js/Components/Articles/ArticleCard/ArticleErrorImage.tsx
@@ -1,17 +1,15 @@
 import cn from "classnames";
-import { useDarkModeContext } from "@/Contexts/DarkModeContext";
 import { ImageLoadError, ImageLoadErrorPrimary } from "@/images";
 
 export const ArticleErrorImage = ({ className }: { className?: string; errorMessage?: string }): JSX.Element => {
-    const { isDark } = useDarkModeContext();
     return (
         <div
             data-testid="ArticleErrorImage"
             className={cn("h-full w-full bg-theme-secondary-100 p-4 dark:bg-theme-primary-800", className)}
         >
             <div className="flex h-full flex-col items-center justify-center">
-                {!isDark && <ImageLoadError className="mx-auto max-w-23" />}
-                {isDark && <ImageLoadErrorPrimary className="mx-auto max-w-23" />}
+                <ImageLoadError className="mx-auto max-w-23 dark:hidden" />
+                <ImageLoadErrorPrimary className="mx-auto hidden max-w-23 dark:block" />
             </div>
         </div>
     );
