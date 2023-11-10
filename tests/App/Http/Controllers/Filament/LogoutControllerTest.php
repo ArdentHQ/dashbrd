ea13c9cd50e7a1f417c29b8dfc18fe8f0e79d80d
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-11-10 00:06:11 -0500

    refactor: LogoutControllerTest

--- tests/App/Http/Controllers/Filament/LogoutControllerTest.php
+++ tests/App/Http/Controllers/Filament/LogoutControllerTest.php
@@ -4,10 +4,27 @@ declare(strict_types=1);
 
 use App\Models\User;
 use App\Http\Controllers\Filament\LogoutController;
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
+use Filament\Facades\Filament;
 
 it('should redirect the admin user to homepage on logout', function () {
     $user = User::factory()->editor()->create();
 
-    $this->actingAs($user)->post(action([LogoutController::class, 'logout']))
-        ->assertRedirect(route('galleries'));
+    Filament::partialMock()
+        ->shouldReceive('auth->logout');
+
+    $this->actingAs($user);
+
+    $request = Request::create('/logout', 'GET');
+    $request->setLaravelSession(Session::driver());
+
+    $controller = new LogoutController();
+
+    $response = $controller->logout($request);
+
+    $this->assertInstanceOf(RedirectResponse::class, $response);
+    $this->assertEquals(route('galleries'), $response->getTargetUrl());
 });
