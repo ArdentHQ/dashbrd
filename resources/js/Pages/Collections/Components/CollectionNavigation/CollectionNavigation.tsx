ed13768ee0f72990af988856e40ea0c674029945
ItsANameToo <ItsANameToo@users.noreply.github.com>
2023-11-10 10:00:15 +0000

    style: resolve style guide violations

--- resources/js/Pages/Collections/Components/CollectionNavigation/CollectionNavigation.tsx
+++ resources/js/Pages/Collections/Components/CollectionNavigation/CollectionNavigation.tsx
@@ -156,7 +156,7 @@ export const CollectionNavigation = ({
                                     <Button
                                         icon="Refresh"
                                         variant="secondary"
-                                        className="hidden bg-theme-secondary-200 dark:bg-theme-dark-700 text-theme-primary-900 sm:flex"
+                                        className="hidden bg-theme-secondary-200 text-theme-primary-900 dark:bg-theme-dark-700 sm:flex"
                                         disabled={!canUpdate(collection)}
                                         onClick={onRefreshActivity}
                                     >
