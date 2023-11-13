38b72d69be9db71e14e691f3e7c329c162c11b3a
goga-m <goga.mamoulasvili@gmail.com>
2023-11-13 15:02:00 +0100

    wip

--- resources/js/Pages/Galleries/hooks/useWalletDraftGalleries.test.ts
+++ resources/js/Pages/Galleries/hooks/useWalletDraftGalleries.test.ts
@@ -1,30 +1,47 @@
 import { expect } from "vitest";
-import { useWalletDraftGalleries } from "./useWalletDraftGalleries";
+import { GalleryDraft, useWalletDraftGalleries } from "./useWalletDraftGalleries";
 import { renderHook, waitFor } from "@/Tests/testing-library";
-
-vi.mock("@/Contexts/AuthContext", () => ({
-    useAuth: () => ({ wallet: { address: "mockedWalletAddress" } }),
-}));
+import { act } from "react-dom/test-utils";
 
 const defaultGalleryDraft = {
     id: 1,
-    walletAddress: "mockedAddress",
-    nfts: [],
     title: "",
     cover: null,
-    coverTye: null,
-    updatedAt: 169901639000,
+    coverType: null,
+    nfts: [],
+    walletAddress: "mockedAddress",
+    value: "test",
+    collectionsCount: 1,
+    updatedAt: new Date().getTime(),
 };
 
+const drafts: GalleryDraft[] = [defaultGalleryDraft];
+
 const indexedDBMocks = {
-    add: vi.fn(),
-    getAll: vi.fn().mockResolvedValue([]),
-    update: vi.fn(),
-    deleteRecord: vi.fn(),
+    add: (draft: GalleryDraft) => {
+        const id = drafts.length + 1;
+        drafts.push({ ...draft, id });
+        return Promise.resolve(id);
+    },
+    getAll: async () => {
+        return Promise.resolve(drafts);
+    },
+    update: async (draft: GalleryDraft) => {
+        const index = drafts.findIndex((savedDraft) => savedDraft.id === draft.id);
+        drafts.splice(index, 1, draft);
+
+        return Promise.resolve(draft);
+    },
+    deleteRecord: async (id: number) => {
+        const index = drafts.findIndex((savedDraft) => savedDraft.id === id);
+        delete drafts[index];
+    },
+    getByID: async (id: number | null) => {
+        return Promise.resolve(drafts.find((draft) => draft.id === id));
+    },
     openCursor: vi.fn(),
     getByIndex: vi.fn(),
     clear: vi.fn(),
-    getByID: vi.fn().mockResolvedValue(defaultGalleryDraft),
 };
 
 const mocks = vi.hoisted(() => ({
@@ -36,11 +53,82 @@ vi.mock("react-indexed-db-hook", () => ({
 }));
 
 describe("useWalletDraftGalleries", () => {
-    it("should add to galleries", async () => {
+    it("should add a new gallery id is not provided", async () => {
         const { result } = renderHook(() => useWalletDraftGalleries({ address: "mockedAddress" }));
 
         await waitFor(() => {
-            expect(result.current.drafts).toHaveLength(0);
+            expect(result.current.isLoading).toBe(false);
         });
+
+        await waitFor(() => {
+            expect(result.current.drafts).toHaveLength(1);
+        });
+
+        await act(async () => {
+            await result.current.upsert({
+                title: "Second Test",
+                cover: null,
+                coverType: null,
+                nfts: [],
+                walletAddress: "mockedAddress",
+                value: "test",
+                collectionsCount: 1,
+                updatedAt: new Date().getTime(),
+            });
+        });
+
+        expect(result.current.isSaving).toBe(false);
+
+        expect(result.current.findById(2)).resolves.toMatchObject(expect.objectContaining({ title: "Second Test" }));
+    });
+
+    it("should update existing draft gallery if id is provided", async () => {
+        const { result } = renderHook(() => useWalletDraftGalleries({ address: "mockedAddress" }));
+
+        await waitFor(() => {
+            expect(result.current.isLoading).toBe(false);
+        });
+
+        await waitFor(() => {
+            expect(result.current.drafts).toHaveLength(2);
+        });
+
+        await act(async () => {
+            await result.current.upsert({
+                id: 1,
+                title: "Test",
+                cover: null,
+                coverType: null,
+                nfts: [],
+                walletAddress: "mockedAddress",
+                value: "test",
+                collectionsCount: 1,
+                updatedAt: new Date().getTime(),
+            });
+        });
+
+        expect(result.current.isSaving).toBe(false);
+        expect(result.current.drafts).toHaveLength(2);
+
+        expect(result.current.findById(1)).resolves.toMatchObject(expect.objectContaining({ title: "Test" }));
+    });
+
+    it("should remove a gallery", async () => {
+        const { result } = renderHook(() => useWalletDraftGalleries({ address: "mockedAddress" }));
+
+        await waitFor(() => {
+            expect(result.current.isLoading).toBe(false);
+        });
+
+        await waitFor(() => {
+            expect(result.current.drafts).toHaveLength(2);
+        });
+
+        await act(async () => {
+            await result.current.remove(2);
+        });
+
+        expect(result.current.isSaving).toBe(false);
+        expect(result.current.drafts).toHaveLength(1);
     });
 });
