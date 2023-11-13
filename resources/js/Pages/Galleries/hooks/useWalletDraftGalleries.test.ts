14912bbd80da32a9baaea8f25a74b212af332583
goga-m <goga.mamoulasvili@gmail.com>
2023-11-13 12:19:30 +0100

    wip

--- resources/js/Pages/Galleries/hooks/useWalletDraftGalleries.test.ts
+++ resources/js/Pages/Galleries/hooks/useWalletDraftGalleries.test.ts
@@ -39,40 +39,6 @@ vi.mock("react-indexed-db-hook", () => ({
 }));
 
 describe("useWalletDraftGalleries", () => {
-    beforeAll(() => {
-        useAuthSpy = vi.spyOn(AuthContextMock, "useAuth").mockReturnValue({
-            user: null,
-            wallet: {
-                address: "mockedAddress",
-                domain: null,
-                totalUsd: 1,
-                totalBalanceInCurrency: "1",
-                totalTokens: 1,
-                collectionCount: 1,
-                galleryCount: 1,
-                timestamps: {
-                    tokens_fetched_at: null,
-                    native_balances_fetched_at: null,
-                },
-                isRefreshingCollections: false,
-                canRefreshCollections: false,
-                avatar: {
-                    small: null,
-                    default: null,
-                    small2x: null,
-                },
-            },
-            authenticated: false,
-            signed: false,
-            logout: vi.fn(),
-            setAuthData: vi.fn(),
-        });
-    });
-
-    afterAll(() => {
-        useAuthSpy.mockRestore();
-    });
-
     it("should add to galleries", async () => {
         const { result } = renderHook(() => useWalletDraftGalleries({ address: "mockedAddress" }));
 
