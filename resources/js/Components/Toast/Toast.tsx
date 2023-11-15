6815605d9066caa6e2107d580f6dc1f3009e7b3d
crnkovic <crnkovic@users.noreply.github.com>
2023-11-15 10:35:25 +0000

    style: resolve style guide violations

--- resources/js/Components/Toast/Toast.tsx
+++ resources/js/Components/Toast/Toast.tsx
@@ -57,7 +57,7 @@ export const Toast = forwardRef<HTMLDivElement, ToastProperties>(
                             />
                         </div>
 
-                        <p>{isExpanded ? (toastTitle ?? title) : message}</p>
+                        <p>{isExpanded ? toastTitle ?? title : message}</p>
                     </div>
 
                     {!isStatic && onClose != null && !isLoading && (
