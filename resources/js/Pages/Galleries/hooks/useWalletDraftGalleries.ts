3687c579740a471d108afc4cbc24b78bdf3642b1
goga-m <goga.mamoulasvili@gmail.com>
2023-11-16 15:03:37 +0100

    wip

--- resources/js/Pages/Galleries/hooks/useWalletDraftGalleries.ts
+++ resources/js/Pages/Galleries/hooks/useWalletDraftGalleries.ts
@@ -46,6 +46,15 @@ interface WalletDraftGalleriesState {
     allDrafts: () => Promise<GallerySavedDraft[]>;
 }
 
+/**
+ * Note: The react-indexed-db-hook  that is used under the hood in this component
+ *       is not reactive. That means that if this hook is used in multiple components,
+ *       the `drafts` state won't re-render on changes happening in indexedDB. `allDrafts` need to be
+ *       explicitly to update the state (applies, for isLoading, isSaving, hasReachedLimit, and `drafts`).
+ *
+ * @param {Properties}
+ * @returns {WalletDraftGalleriesState}
+ */
 export const useWalletDraftGalleries = ({ address }: Properties): WalletDraftGalleriesState => {
     const database = useIndexedDB("gallery-drafts");
     const [drafts, setDrafts] = useState<GalleryDraft[]>([]);
