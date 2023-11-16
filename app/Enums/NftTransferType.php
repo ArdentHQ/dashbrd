d0bb50b02cee7e4439023242ce860066fd0dbd1d
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2023-11-16 08:43:42 +0100

    wip

--- app/Enums/NftTransferType.php
+++ app/Enums/NftTransferType.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 namespace App\Enums;
 
-use InvalidArgumentException;
 use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 
-/**
- * @see https://docs.mnemonichq.com/references/uniform/rest/reference/#operation/FoundationalService_GetNftTransfers
- */
 #[TypeScript]
 enum NftTransferType: string
 {
@@ -17,15 +13,4 @@ enum NftTransferType: string
     case Sale = 'LABEL_SALE';
     case Transfer = 'LABEL_TRANSFER';
     case Burn = 'LABEL_BURN';
-
-    public static function getEnumFromValue(string $value): self
-    {
-        return match ($value) {
-            'LABEL_MINT' => self::Mint,
-            'LABEL_SALE' => self::Sale,
-            'LABEL_TRANSFER' => self::Transfer,
-            'LABEL_BURN' => self::Burn,
-            default => throw new InvalidArgumentException(sprintf('Invalid value: %s', $value)),
-        };
-    }
 }
