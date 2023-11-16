6676c2db10d2362072324dcaee132a05f23a1279
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2023-11-16 12:27:51 +0100

    wip

---
+++ resources/js/Components/Galleries/GalleryPage/GalleryActionToolbar/GalleryDraftDeleteButton.tsx
@@ -0,0 +1,52 @@
+import { router } from "@inertiajs/core";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { IconButton } from "@/Components/Buttons";
+import { ConfirmDeletionDialog } from "@/Components/ConfirmDeletionDialog";
+import { useAuth } from "@/Contexts/AuthContext";
+import { useWalletDraftGalleries } from "@/Pages/Galleries/hooks/useWalletDraftGalleries";
+import { assertWallet } from "@/Utils/assertions";
+
+export const GalleryDraftDeleteButton = ({ draftId }: { draftId: number }): JSX.Element => {
+    const { t } = useTranslation();
+    const [open, setOpen] = useState(false);
+
+    const { wallet } = useAuth();
+
+    assertWallet(wallet);
+
+    const { remove } = useWalletDraftGalleries({ address: wallet.address });
+
+    return (
+        <>
+            <IconButton
+                data-testid="GalleryActionToolbar__draftDelete"
+                icon="Trash"
+                className="flex sm:hidden lg:flex"
+                onClick={() => {
+                    setOpen(true);
+                }}
+            />
+
+            <ConfirmDeletionDialog
+                title={t("pages.galleries.delete_modal.title")}
+                isOpen={open}
+                onClose={() => {
+                    setOpen(false);
+                }}
+                onConfirm={() => {
+                    void remove(draftId);
+
+                    router.visit(
+                        route("my-galleries", {
+                            draft: true,
+                        }),
+                    );
+                }}
+                isDisabled={false}
+            >
+                {t("pages.galleries.delete_modal.confirmation_text")}
+            </ConfirmDeletionDialog>
+        </>
+    );
+};
