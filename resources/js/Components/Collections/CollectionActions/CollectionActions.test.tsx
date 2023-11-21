99426df6000119c48b51078528954983d561c447
goga-m <goga.mamoulasvili@gmail.com>
2023-11-21 14:59:47 +0100

    wip

--- resources/js/Components/Collections/CollectionActions/CollectionActions.test.tsx
+++ resources/js/Components/Collections/CollectionActions/CollectionActions.test.tsx
@@ -28,7 +28,107 @@ describe("CollectionActions", () => {
         useAuthorizedActionSpy.mockRestore();
     });
 
-    it.each([true, false])("should render", (isHidden) => {
+    it("should report a collection", async () => {
+        signedActionMock.mockImplementation((action) => {
+            action({ authenticated: true, signed: true });
+        });
+
+        const onReportMock = vi.fn();
+
+        const t: unknown = {};
+        const routerSpy = vi.spyOn(router, "post").mockImplementation((_, __, options?: VisitOptions) => {
+            options?.onSuccess?.(t as Page<PageProps>);
+        });
+
+        render(
+            <CollectionActions
+                collection={collection}
+                isHidden={false}
+                onChanged={vi.fn()}
+                onReportCollection={onReportMock}
+                reportReasons={{ reason: "reason" }}
+            />,
+        );
+
+        expect(screen.queryByTestId("ReportModal")).not.toBeInTheDocument();
+
+        await userEvent.click(screen.getByTestId("CollectionActions__trigger"));
+
+        expect(screen.queryByTestId("CollectionActions__popup")).toBeInTheDocument();
+
+        const reportButton = screen.getByTestId("CollectionActions__report");
+
+        await userEvent.click(reportButton);
+
+        await waitFor(() => {
+            expect(screen.getByTestId("Dialog")).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            expect(screen.getByTestId("ConfirmationDialog__form")).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            expect(screen.getByTestId("ReportModal")).toBeInTheDocument();
+        });
+
+        expect(screen.getAllByTestId("ReportModal__radio")).toHaveLength(1);
+
+        await userEvent.click(screen.getByTestId("ReportModal__radio"));
+
+        const confirmButton = screen.getByTestId("ConfirmationDialog__confirm");
+
+        await userEvent.click(confirmButton);
+
+        expect(screen.queryByTestId("ReportModal")).not.toBeInTheDocument();
+        expect(routerSpy).toHaveBeenCalled();
+    });
+
+    it("can show and manage report modal state if wallet is signed", async () => {
+        signedActionMock.mockImplementation((action) => {
+            action({ authenticated: true, signed: true });
+        });
+
+        const routerSpy = vi.spyOn(router, "reload").mockImplementation(() => vi.fn());
+
+        render(
+            <CollectionActions
+                collection={collection}
+                isHidden={false}
+                onChanged={vi.fn()}
+            />,
+        );
+
+        expect(screen.queryByTestId("ReportModal")).not.toBeInTheDocument();
+
+        await userEvent.click(screen.getByTestId("CollectionActions__trigger"));
+
+        expect(screen.queryByTestId("CollectionActions__popup")).toBeInTheDocument();
+
+        const reportButton = screen.getByTestId("CollectionActions__report");
+
+        await userEvent.click(reportButton);
+
+        await waitFor(() => {
+            expect(screen.getByTestId("Dialog")).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            expect(screen.getByTestId("ConfirmationDialog__form")).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            expect(screen.getByTestId("ReportModal")).toBeInTheDocument();
+        });
+
+        const closeButton = screen.getByTestId("ConfirmationDialog__close");
+
+        await userEvent.click(closeButton);
+
+        expect(screen.queryByTestId("ReportModal")).not.toBeInTheDocument();
+    });
+
+    it.each([true, false])(`should render with hidden %s`, (isHidden) => {
         render(
             <CollectionActions
                 collection={collection}
@@ -219,93 +319,5 @@ describe("CollectionActions", () => {
         expect(routerSpy).toHaveBeenCalled();
 
         routerSpy.mockRestore();
-    });
-
-    it("can show and manage report modal state if wallet is signed", async () => {
-        signedActionMock.mockImplementation((action) => {
-            action({ authenticated: true, signed: true });
-        });
-
-        render(
-            <CollectionActions
-                collection={collection}
-                isHidden={false}
-                onChanged={vi.fn()}
-            />,
-        );
-
-        expect(screen.queryByTestId("ReportModal")).not.toBeInTheDocument();
-
-        await userEvent.click(screen.getByTestId("CollectionActions__trigger"));
-
-        expect(screen.queryByTestId("CollectionActions__popup")).toBeInTheDocument();
-
-        const reportButton = screen.getByTestId("CollectionActions__report");
-
-        await userEvent.click(reportButton);
-
-        await waitFor(
-            () => {
-                expect(screen.getByTestId("ReportModal")).toBeInTheDocument();
-            },
-            { timeout: 2000 },
-        );
-
-        const closeButton = screen.getByTestId("ConfirmationDialog__close");
-
-        await userEvent.click(closeButton);
-
-        expect(screen.queryByTestId("ReportModal")).not.toBeInTheDocument();
-    });
-
-    it("should report a collection", async () => {
-        signedActionMock.mockImplementation((action) => {
-            action({ authenticated: true, signed: true });
-        });
-
-        const onReportMock = vi.fn();
-
-        const t: unknown = {};
-        const routerSpy = vi.spyOn(router, "post").mockImplementation((_, __, options?: VisitOptions) => {
-            options?.onSuccess?.(t as Page<PageProps>);
-        });
-
-        render(
-            <CollectionActions
-                collection={collection}
-                isHidden={false}
-                onChanged={vi.fn()}
-                onReportCollection={onReportMock}
-                reportReasons={{ reason: "reason" }}
-            />,
-        );
-
-        expect(screen.queryByTestId("ReportModal")).not.toBeInTheDocument();
-
-        await userEvent.click(screen.getByTestId("CollectionActions__trigger"));
-
-        expect(screen.queryByTestId("CollectionActions__popup")).toBeInTheDocument();
-
-        const reportButton = screen.getByTestId("CollectionActions__report");
-
-        await userEvent.click(reportButton);
-
-        await waitFor(
-            () => {
-                expect(screen.getByTestId("ReportModal")).toBeInTheDocument();
-            },
-            { timeout: 2000 },
-        );
-
-        expect(screen.getAllByTestId("ReportModal__radio")).toHaveLength(1);
-
-        await userEvent.click(screen.getByTestId("ReportModal__radio"));
-
-        const confirmButton = screen.getByTestId("ConfirmationDialog__confirm");
-
-        await userEvent.click(confirmButton);
-
-        expect(screen.queryByTestId("ReportModal")).not.toBeInTheDocument();
-        expect(routerSpy).toHaveBeenCalled();
     });
 });
