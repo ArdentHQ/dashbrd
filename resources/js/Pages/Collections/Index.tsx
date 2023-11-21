3fae3d6cd5a176854cdaff93cbb5ac47c6bcc1e6
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-11-21 07:41:20 -0500

    feat: initial implementation for FeaturedCollectionsSlider

--- resources/js/Pages/Collections/Index.tsx
+++ resources/js/Pages/Collections/Index.tsx
@@ -1,12 +1,22 @@
 import { Head, usePage } from "@inertiajs/react";
+import { FeaturedCollectionsSlider } from "./Components/FeaturedCollections";
 import { DefaultLayout } from "@/Layouts/DefaultLayout";
 
-const CollectionsIndex = ({ title }: { title: string }): JSX.Element => {
+const CollectionsIndex = ({
+    title,
+    featuredCollections,
+}: {
+    title: string;
+    featuredCollections: App.Data.Collections.CollectionFeaturedData[];
+}): JSX.Element => {
     const { props } = usePage();
 
     return (
         <DefaultLayout toastMessage={props.toast}>
             <Head title={title} />
+            <div>
+                <FeaturedCollectionsSlider featuredCollections={featuredCollections} />
+            </div>
         </DefaultLayout>
     );
 };
