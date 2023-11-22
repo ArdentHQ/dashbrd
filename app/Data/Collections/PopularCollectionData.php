2b6106f9c2e98df31223b3de8415e05fd1ae6f8a
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-11-22 10:24:06 -0600

    cleanup

--- app/Data/Collections/PopularCollectionData.php
+++ app/Data/Collections/PopularCollectionData.php
@@ -10,32 +10,26 @@ use App\Transformers\IpfsGatewayUrlTransformer;
 use Illuminate\Support\Str;
 use Spatie\LaravelData\Attributes\WithTransformer;
 use Spatie\LaravelData\Data;
-use Spatie\LaravelData\DataCollection;
 use Spatie\TypeScriptTransformer\Attributes\LiteralTypeScriptType;
 use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 
 #[TypeScript]
 class PopularCollectionData extends Data
 {
-    /**
-     * @param  DataCollection<int, SimpleNftData>  $nfts
-     */
     public function __construct(
         public int $id,
         public string $name,
         public string $slug,
         #[LiteralTypeScriptType('App.Enums.Chain')]
         public int $chainId,
-        #[WithTransformer(IpfsGatewayUrlTransformer::class)]
         public ?string $floorPrice,
         public ?string $floorPriceCurrency,
         public ?int $floorPriceDecimals,
-
         public ?string $volume,
         public ?float $volumeFiat,
         public ?string $volumeCurrency,
         public ?int $volumeDecimals,
-
+        #[WithTransformer(IpfsGatewayUrlTransformer::class)]
         public ?string $image,
     ) {
     }
@@ -50,13 +44,11 @@ class PopularCollectionData extends Data
             floorPrice: $collection->floor_price,
             floorPriceCurrency: $collection->floorPriceToken ? Str::lower($collection->floorPriceToken->symbol) : null,
             floorPriceDecimals: $collection->floorPriceToken?->decimals,
-
             // @TODO: makey this dynamic
             volume: '19000000000000000000',
             volumeFiat: 35380.4,
             volumeCurrency: 'eth',
             volumeDecimals: 18,
-
             image: $collection->extra_attributes->get('image'),
         );
     }
--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -28,6 +28,7 @@ use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Inertia\Inertia;
 use Inertia\Response;
 use Spatie\LaravelData\PaginatedDataCollection;
