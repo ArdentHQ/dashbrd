7ee687fa2fe695cd7c085f58aa4537c1743648b2
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-11-22 09:31:10 -0600

    remove sorting

--- resources/js/Components/Collections/PopularCollectionsTable/PopularCollectionsTable.tsx
+++ resources/js/Components/Collections/PopularCollectionsTable/PopularCollectionsTable.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { type Column, type TableState } from "react-table";
+import { type Column } from "react-table";
 import { type PopularCollectionTableProperties } from "./PopularCollectionsTable.contract";
 import { PopularCollectionsTableItem } from "./PopularCollectionsTableItem";
 import { Table } from "@/Components/Table";
@@ -12,24 +12,11 @@ export const PopularCollectionsTable = ({ collections, user }: PopularCollection
 
     const sortDirection = "desc";
 
-    const initialState = useMemo<Partial<TableState<App.Data.Collections.PopularCollectionData>>>(
-        () => ({
-            sortBy: [
-                {
-                    desc: true,
-                    id: activeSort,
-                },
-            ],
-        }),
-        [],
-    );
-
     const columns = useMemo(() => {
         const columns: Array<Column<App.Data.Collections.PopularCollectionData>> = [
             {
                 Header: t("common.collection").toString(),
                 id: "name",
-                accessor: (collection) => collection.name,
                 className: "justify-start",
                 cellWidth: "sm:w-full",
                 paddingClassName: "py-2 px-2 md:px-5",
@@ -37,15 +24,11 @@ export const PopularCollectionsTable = ({ collections, user }: PopularCollection
             {
                 Header: t("common.floor_price").toString(),
                 id: "floor-price",
-                accessor: (collection) => collection.floorPriceFiat,
                 className: "justify-end whitespace-nowrap",
             },
             {
                 Header: t("common.volume").toString(),
                 id: "volume",
-                // @TODO: change this
-                accessor: (collection) => collection.floorPriceFiat,
-                className: "justify-end whitespace-nowrap",
             },
         ];
 
@@ -61,7 +44,6 @@ export const PopularCollectionsTable = ({ collections, user }: PopularCollection
             data-testid="PopularCollectionsTable"
             variant="list"
             columns={columns}
-            initialState={initialState}
             activeSort={activeSort}
             sortDirection={sortDirection}
             manualSortBy={true}
