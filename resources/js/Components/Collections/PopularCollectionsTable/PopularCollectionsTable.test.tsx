e5f5fe6f0c9a43145450a2f5416c57824008957c
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-11-22 10:49:16 -0600

    test coverage

--- resources/js/Components/Collections/PopularCollectionsTable/PopularCollectionsTable.test.tsx
+++ resources/js/Components/Collections/PopularCollectionsTable/PopularCollectionsTable.test.tsx
@@ -1,9 +1,10 @@
+import { router } from "@inertiajs/react";
 import { type SpyInstance } from "vitest";
 import { PopularCollectionsTable } from "./PopularCollectionsTable";
 import * as useAuthorizedActionMock from "@/Hooks/useAuthorizedAction";
 import PopularCollectionFactory from "@/Tests/Factories/Collections/PopularCollectionFactory";
 import UserDataFactory from "@/Tests/Factories/UserDataFactory";
-import { render } from "@/Tests/testing-library";
+import { render, userEvent } from "@/Tests/testing-library";
 import { allBreakpoints } from "@/Tests/utils";
 
 let useAuthorizedActionSpy: SpyInstance;
@@ -25,7 +26,7 @@ describe("PopularCollectionsTable", () => {
         useAuthorizedActionSpy.mockRestore();
     });
 
-    const collections = new PopularCollectionFactory().createMany(3);
+    const collections = new PopularCollectionFactory().withPrices().withVolume().createMany(3);
 
     const user = new UserDataFactory().create();
 
@@ -39,5 +40,50 @@ describe("PopularCollectionsTable", () => {
         );
 
         expect(getByTestId("PopularCollectionsTable")).toBeInTheDocument();
+    });
+
+    it("renders nothing if no collections", () => {
+        const { queryByTestId } = render(
+            <PopularCollectionsTable
+                collections={[]}
+                user={user}
+            />,
+        );
+
+        expect(queryByTestId("PopularCollectionsTable")).not.toBeInTheDocument();
+    });
+
+    it("renders without floor price and volume", () => {
+        const collections = new PopularCollectionFactory().withoutPrices().withoutVolume().createMany(3);
+
+        const { getByTestId } = render(
+            <PopularCollectionsTable
+                collections={collections}
+                user={user}
+            />,
+        );
+
+        expect(getByTestId("PopularCollectionsTable")).toBeInTheDocument();
+    });
+
+    it("visits the collection page on row click", async () => {
+        const function_ = vi.fn();
+
+        const routerSpy = vi.spyOn(router, "visit").mockImplementation(function_);
+
+        const { getByTestId, getAllByTestId } = render(
+            <PopularCollectionsTable
+                collections={collections}
+                user={user}
+            />,
+        );
+
+        expect(getByTestId("PopularCollectionsTable")).toBeInTheDocument();
+
+        await userEvent.click(getAllByTestId("TableRow")[0]);
+
+        expect(routerSpy).toHaveBeenCalled();
+
+        routerSpy.mockRestore();
     });
 });
--- resources/js/Tests/Factories/Collections/PopularCollectionFactory.ts
+++ resources/js/Tests/Factories/Collections/PopularCollectionFactory.ts
@@ -18,4 +18,38 @@ export default class PopularCollectionFactory extends ModelFactory<App.Data.Coll
             image: this.optional(faker.image.avatar(), 0.9),
         };
     }
+
+    withPrices(): this {
+        return this.state(() => ({
+            floorPrice: faker.finance.amount(1 * 1e18, 25 * 1e18, 0),
+            floorPriceCurrency: this.cryptoCurrency(),
+            floorPriceDecimals: 18,
+        }));
+    }
+
+    withoutPrices(): this {
+        return this.state(() => ({
+            floorPrice: null,
+            floorPriceCurrency: null,
+            floorPriceDecimals: null,
+        }));
+    }
+
+    withVolume(): this {
+        return this.state(() => ({
+            volume: faker.finance.amount(1 * 1e18, 25 * 1e18, 0),
+            volumeFiat: Number(faker.finance.amount(1, 1500, 2)),
+            volumeCurrency: this.cryptoCurrency(),
+            volumeDecimals: 18,
+        }));
+    }
+
+    withoutVolume(): this {
+        return this.state(() => ({
+            volume: null,
+            volumeFiat: null,
+            volumeCurrency: null,
+            volumeDecimals: null,
+        }));
+    }
 }
