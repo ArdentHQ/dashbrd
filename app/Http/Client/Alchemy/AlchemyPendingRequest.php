802dd46ecbbcdd9a5feea78a9ae3a6ab0f4a5da7
shahin-hq <132887516+shahin-hq@users.noreply.github.com>
2023-11-22 13:05:08 +0400

    refactor: migrate `getFloorPrice` to Alchemy v3 (#468)

--- app/Http/Client/Alchemy/AlchemyPendingRequest.php
+++ app/Http/Client/Alchemy/AlchemyPendingRequest.php
@@ -498,7 +498,7 @@ class AlchemyPendingRequest extends PendingRequest
             return null;
         }
 
-        $this->apiUrl = $this->getNftV2ApiUrl();
+        $this->apiUrl = $this->getNftV3ApiUrl();
         $this->chain = AlchemyChain::fromChainId($chain->value);
 
         /** @var array<string, mixed> $data */
@@ -520,18 +520,20 @@ class AlchemyPendingRequest extends PendingRequest
         //     "floorPrice": 1.235,
         //     "priceCurrency": "ETH",
         //     "retrievedAt": "2023-03-30T04:08:09.791Z",
-        //     "collectionUrl": "https://opensea.io/collection/world-of-women-nft"
+        //     "collectionUrl": "https://opensea.io/collection/world-of-women-nft",
+        //     "error": null
         //   },
         //   "looksRare": {
         //     "floorPrice": 1.2472,
         //     "priceCurrency": "ETH",
         //     "retrievedAt": "2023-03-30T03:59:09.707Z",
         //     "collectionUrl": "https://looksrare.org/collections/0xe785e82358879f061bc3dcac6f0444462d4b5330"
+        //     "error": null
         //   }
         // }
         /** @var array<string, mixed> | null $priceInfo */
         $priceInfo = collect($data)->first(function ($k, $v) {
-            return ! Arr::has($k, 'error');
+            return Arr::get($k, 'error', 1) === null;
         });
 
         if (empty($priceInfo)) {
