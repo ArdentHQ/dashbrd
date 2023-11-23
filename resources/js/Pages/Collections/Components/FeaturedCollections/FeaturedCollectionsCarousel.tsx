04184bf90d907bda80454c8be67c38c76ee21834
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-11-23 09:28:09 -0500

    fix: remove fade effect from carousel

--- resources/js/Pages/Collections/Components/FeaturedCollections/FeaturedCollectionsCarousel.tsx
+++ resources/js/Pages/Collections/Components/FeaturedCollections/FeaturedCollectionsCarousel.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Autoplay, EffectFade, Navigation, Pagination } from "swiper";
+import { Autoplay, Navigation, Pagination } from "swiper";
 import { FeaturedCollectionsItem } from "./FeaturedCollectionsItem";
 import { Carousel, CarouselItem } from "@/Components/Carousel";
 
@@ -12,14 +12,12 @@ export const FeaturedCollectionsCarousel = ({
         shouldShowHeader={false}
         loop={true}
         className="overflow-hidden lg:mx-8 lg:rounded-xl 2xl:mx-0"
-        effect={"fade"}
-        speed={1000}
         navigation={true}
         autoplay={{
             delay: 5000,
             pauseOnMouseEnter: true,
         }}
-        modules={[Navigation, Pagination, EffectFade, Autoplay]}
+        modules={[Navigation, Pagination, Autoplay]}
     >
         {featuredCollections.map((collection, index) => (
             <CarouselItem key={index}>
