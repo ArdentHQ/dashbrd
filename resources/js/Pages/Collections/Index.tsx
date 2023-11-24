34ae9a7beb6283128f0f95836914f7bec4111fd4
Josip Crnković <crnkovic@users.noreply.github.com>
2023-11-24 11:38:11 +0100

    refactor: add "view all" button to popular collections (#492)

--- resources/js/Pages/Collections/Index.tsx
+++ resources/js/Pages/Collections/Index.tsx
@@ -2,6 +2,7 @@ import { type PageProps } from "@inertiajs/core";
 import { Head, usePage } from "@inertiajs/react";
 import { useTranslation } from "react-i18next";
 import { PopularCollectionsSorting } from "./Components/PopularCollectionsSorting";
+import { ButtonLink } from "@/Components/Buttons/ButtonLink";
 import { PopularCollectionsTable } from "@/Components/Collections/PopularCollectionsTable";
 import { Heading } from "@/Components/Heading";
 import { type PaginationData } from "@/Components/Pagination/Pagination.contracts";
@@ -32,29 +33,61 @@ const CollectionsIndex = ({
             <Head title={title} />
 
             <div className="mx-6 sm:mx-8 2xl:mx-0">
-                <Heading level={1}>{t("pages.collections.popular_collections")}</Heading>
+                <div className="flex items-center justify-between">
+                    <Heading level={1}>{t("pages.collections.popular_collections")}</Heading>
 
-                <div className="mt-4">
-                    <PopularCollectionsSorting active={activeSort} />
+                    <div className="hidden sm:block lg:hidden">
+                        <ViewAllButton />
+                    </div>
                 </div>
 
-                <div className="flex sm:space-x-2 md:space-x-3 lg:space-x-6">
-                    <div className="flex-1">
-                        <PopularCollectionsTable
-                            collections={collectionsColumn1}
-                            user={auth.user}
-                        />
+                <div className="mt-4 hidden items-center justify-between lg:flex">
+                    <div>
+                        <PopularCollectionsSorting active={activeSort} />
                     </div>
 
-                    <div className="hidden flex-1 sm:block">
-                        <PopularCollectionsTable
-                            collections={collectionsColumn2}
-                            user={auth.user}
-                        />
+                    <div>
+                        <ViewAllButton />
+                    </div>
+                </div>
+
+                <div>
+                    <div className="flex sm:space-x-2 md:space-x-3 lg:space-x-6">
+                        <div className="flex-1">
+                            <PopularCollectionsTable
+                                collections={collectionsColumn1}
+                                user={auth.user}
+                            />
+                        </div>
+
+                        <div className="hidden flex-1 sm:block">
+                            <PopularCollectionsTable
+                                collections={collectionsColumn2}
+                                user={auth.user}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="mt-2 sm:hidden">
+                        <ViewAllButton />
                     </div>
                 </div>
             </div>
         </DefaultLayout>
+    );
+};
+
+const ViewAllButton = (): JSX.Element => {
+    const { t } = useTranslation();
+
+    return (
+        <ButtonLink
+            variant="secondary"
+            href="#"
+            className="w-full justify-center sm:w-auto"
+        >
+            {t("common.view_all")}
+        </ButtonLink>
     );
 };
 
