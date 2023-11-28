326e03302620e2b2dd4c217fca38a51578cd07e4
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-11-28 07:35:23 -0500

    fix: display placeholder for articles tab

--- resources/js/Pages/Articles/Components/ArticlesView.tsx
+++ resources/js/Pages/Articles/Components/ArticlesView.tsx
@@ -132,7 +132,7 @@ export const ArticlesView = ({
 
                 {isLoading && displayType === DisplayTypes.List && <ArticlesLoadingList />}
 
-                {!isLoading && articlesToShow.length === 0 && highlightedArticles?.length === 0 && query === "" && (
+                {!isLoading && articlesToShow.length === 0 && (!highlightedArticles || highlightedArticles?.length === 0) && query === "" && (
                     <EmptyBlock className="w-full">
                         {mode === "articles"
                             ? t("pages.articles.no_articles")
