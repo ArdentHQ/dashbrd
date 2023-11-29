ee2271acb53e83f909f10fc51e4107f6deb217f6
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2023-11-29 14:31:11 +0100

    Revert "wip"
    This reverts commit ea635561c4b45e7f3fc18115adc0b82d20b44070.

--- tests/App/Jobs/FetchWalletNftsTest.php
+++ tests/App/Jobs/FetchWalletNftsTest.php
@@ -39,7 +39,7 @@ it('should fetch nfts for wallet', function () {
 
     (new FetchWalletNfts($wallet, $network))->handle();
 
-    $this->assertDatabaseCount('collections', 42);
+    $this->assertDatabaseCount('collections', 47);
     $this->assertDatabaseCount('nfts', 94);
 
     expect(Collection::whereNotNull('last_indexed_token_number')->count())->toBe(0);
@@ -77,10 +77,10 @@ it('should fetch nfts for wallet and dispatch floor price job', function () {
 
     (new FetchWalletNfts($wallet, $network))->handle();
 
-    $this->assertDatabaseCount('collections', 42);
+    $this->assertDatabaseCount('collections', 47);
     $this->assertDatabaseCount('nfts', 94);
 
-    Bus::assertDispatchedTimes(FetchCollectionFloorPrice::class, 42);
+    Bus::assertDispatchedTimes(FetchCollectionFloorPrice::class, 47);
 });
 
 it('should fetch nfts for wallet and skip floor price job if already present', function () {
@@ -707,7 +707,7 @@ it('should clear gallery & user cache', function () {
     // Fetch same NFTs with a different wallet
     (new FetchWalletNfts($wallet1, $network))->handle();
 
-    $this->assertDatabaseCount('collections', 42);
+    $this->assertDatabaseCount('collections', 47);
     $this->assertDatabaseCount('nfts', 94);
     $this->assertDatabaseCount('galleries_dirty', 0);
 
@@ -1224,7 +1224,7 @@ it('should fetch nfts for wallet and keep previous collections last indexed toke
 
     (new FetchWalletNfts($wallet, $network))->handle();
 
-    $this->assertDatabaseCount('collections', 42);
+    $this->assertDatabaseCount('collections', 47);
     $this->assertDatabaseCount('nfts', 94);
 
     expect(Collection::whereNotNull('last_indexed_token_number')->count())->toBe(1);
