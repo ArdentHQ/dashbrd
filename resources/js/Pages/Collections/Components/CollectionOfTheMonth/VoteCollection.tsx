843e42bed6efbe59365a19b4c542bc538719032f
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-12-01 10:17:02 -0500

    feat: implement VoteDialog

--- resources/js/Pages/Collections/Components/CollectionOfTheMonth/VoteCollection.tsx
+++ resources/js/Pages/Collections/Components/CollectionOfTheMonth/VoteCollection.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { VoteCountdown } from "./VoteCountdown";
+import VoteDialog from "./VoteDialog";
 import { Heading } from "@/Components/Heading";
 import { LinkButton } from "@/Components/Link";
 
 export const VoteCollection = (): JSX.Element => {
     const { t } = useTranslation();
+
+    const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
 
     return (
         <div className="flex w-full flex-col gap-4 rounded-xl border-theme-secondary-300 p-0 dark:border-theme-dark-700 lg:border lg:p-8">
@@ -16,7 +19,7 @@ export const VoteCollection = (): JSX.Element => {
 
                 <LinkButton
                     onClick={(): void => {
-                        console.log("TODO: Implement or nominate collection");
+                        setIsDialogOpen(true);
                     }}
                     variant="link"
                     className="font-medium leading-6 dark:hover:decoration-theme-primary-400"
@@ -26,6 +29,11 @@ export const VoteCollection = (): JSX.Element => {
                     {t("pages.collections.vote.or_nominate_collection")}
                 </LinkButton>
             </div>
+
+            <VoteDialog
+                isOpen={isDialogOpen}
+                setIsOpen={setIsDialogOpen}
+            />
         </div>
     );
 };
