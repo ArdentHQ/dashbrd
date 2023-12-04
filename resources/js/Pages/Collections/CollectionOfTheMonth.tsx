c1264d1836b78ef94803d29716e59c72e5ba4fe8
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-12-04 17:09:11 -0600

    add back to collections link

--- resources/js/Pages/Collections/CollectionOfTheMonth.tsx
+++ resources/js/Pages/Collections/CollectionOfTheMonth.tsx
@@ -1,6 +1,9 @@
-import { type PageProps } from "@inertiajs/core";
+import { type PageProps, router } from "@inertiajs/core";
 import { Head } from "@inertiajs/react";
+import cn from "classnames";
 import { useTranslation } from "react-i18next";
+import { IconButton } from "@/Components/Buttons";
+import { Link } from "@/Components/Link";
 import { DefaultLayout } from "@/Layouts/DefaultLayout";
 
 interface CollectionOfTheMonthProperties extends PageProps {
@@ -13,6 +16,33 @@ const CollectionOfTheMonth = ({ title }: CollectionOfTheMonthProperties): JSX.El
     return (
         <DefaultLayout wrapperClassName="-mt-6 sm:-mt-8 lg:mt-0 -mb-6 sm:-mb-8 lg:mb-0">
             <Head title={title} />
+
+            <div className="flex items-center">
+                <IconButton
+                    icon="ChevronLeftSmall"
+                    onClick={() => {
+                        router.get(route("collections"));
+                    }}
+                    iconSize="xs"
+                    className="mr-3 text-theme-dark-300"
+                />
+                <span>
+                    <span className="font-medium text-theme-secondary-700 dark:text-theme-dark-200">
+                        {t("common.back_to")}{" "}
+                    </span>
+                    <Link
+                        href={route("collections")}
+                        className={cn(
+                            "transition-default font-medium text-theme-primary-600 underline decoration-transparent underline-offset-2 outline-none",
+                            "hover:text-theme-primary-700 hover:decoration-theme-primary-700",
+                            "focus-visible:decoration-theme-primary-700",
+                            "dark:text-theme-primary-400 dark:hover:text-theme-primary-500 dark:hover:decoration-theme-primary-500",
+                        )}
+                    >
+                        {t("common.collections")}
+                    </Link>
+                </span>
+            </div>
 
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae rerum exercitationem qui numquam
