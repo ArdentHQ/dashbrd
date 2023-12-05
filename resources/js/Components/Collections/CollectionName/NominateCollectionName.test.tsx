5e125c4097a33fe5d7cbd15d119abaf6e0b9f665
Juan Patricio Marroquin <juan.marroquin1@unmsm.edu.pe>
2023-12-05 10:24:20 -0500

    fix: coverage for NominateCollectionName

--- resources/js/Components/Collections/CollectionName/NominateCollectionName.test.tsx
+++ resources/js/Components/Collections/CollectionName/NominateCollectionName.test.tsx
@@ -3,7 +3,7 @@ import { NominateCollectionName } from "./NominateCollectionName";
 import PopularCollectionFactory from "@/Tests/Factories/Collections/PopularCollectionFactory";
 import { render, screen } from "@/Tests/testing-library";
 
-describe("CollectionName", () => {
+describe("NominateCollectionName", () => {
     it("should render", () => {
         const collection = new PopularCollectionFactory().create();
 
@@ -44,5 +44,29 @@ describe("CollectionName", () => {
         render(<NominateCollectionName collection={collection} />);
 
         expect(screen.getByTestId("CollectionName__volume")).toHaveTextContent("0 BTC");
+    });
+
+    it("should render the volume using 18 decimals to format by default", () => {
+        const collection = new PopularCollectionFactory().create({
+            volume: "1000000000000000000",
+            volumeCurrency: "ETH",
+            volumeDecimals: undefined,
+        });
+
+        render(<NominateCollectionName collection={collection} />);
+
+        expect(screen.getByTestId("CollectionName__volume")).toHaveTextContent("1 ETH");
+    });
+
+    it("should render the volume using the specified decimals", () => {
+        const collection = new PopularCollectionFactory().create({
+            volume: "1000000000000000000",
+            volumeCurrency: "ETH",
+            volumeDecimals: 6,
+        });
+
+        render(<NominateCollectionName collection={collection} />);
+
+        expect(screen.getByTestId("CollectionName__volume")).toHaveTextContent("1,000,000,000,000 ETH");
     });
 });
