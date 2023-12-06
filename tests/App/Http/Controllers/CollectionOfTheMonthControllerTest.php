618d4ac00dd803470bd2b14952a5bf82284b2991
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-12-06 13:47:00 -0600

    Update CollectionOfTheMonthControllerTest.php

--- tests/App/Http/Controllers/CollectionOfTheMonthControllerTest.php
+++ tests/App/Http/Controllers/CollectionOfTheMonthControllerTest.php
@@ -3,9 +3,17 @@
 declare(strict_types=1);
 
 use App\Models\Collection;
+use App\Models\CollectionVote;
 
 it('opens the collection of the month page', function () {
+    $collection = Collection::factory()->create();
+
+    CollectionVote::factory()->create(['collection_id' => $collection->id]);
+
     $this->get(route('collection-of-the-month'))->assertOk();
+});
+it('shows a 404 page if no collections', function () {
+    $this->get(route('collection-of-the-month'))->assertNotFound();
 });
 
 it('prevents having a collection with `collection-of-the-month` as slug', function () {
