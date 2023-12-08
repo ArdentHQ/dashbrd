e1caed2880f5b5f6fe23d38b43cdd6690c4c353b
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-12-08 17:24:21 -0600

    add volume columns

---
+++ database/migrations/2023_12_08_232212_add_volume_periods_columns_to_collections_table.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('collections', function (Blueprint $table) {
+            $table->string('volume_7d')->nullable();
+            $table->string('volume_1m')->nullable();
+        });
+    }
+};
