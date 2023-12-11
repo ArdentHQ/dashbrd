e3eaf51ec69aa83ed6815836038c9dd076ca690e
goga-m <goga.mamoulasvili@gmail.com>
2023-12-11 18:03:12 +0100

    wip

---
+++ database/seeders/WinnerCollectionSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\Collection;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class WinnerCollectionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $this->generateWinners(Carbon::now()->year);
+    }
+
+    public function generateWinners(int $year): void
+    {
+
+        for ($month = 1; $month <= 12; $month++) {
+
+            $monthlyWinnerCollections = Collection::query()
+                ->whereNull("has_won_at")
+                ->limit(3)
+                ->get();
+
+            $currentDate = Carbon::createFromDate($year, $month, 1);
+
+            $monthlyWinnerCollections->each(function ($collection) use ($currentDate) {
+                $collection->update(['has_won_at' => $currentDate->toString()]);
+            });
+
+        }
+    }
+}
