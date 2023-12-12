9f45bb3fb52e7894cb4ae9abc5bd2c7b3451aa51
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-12-12 11:20:08 -0600

    Update 2023_12_11_202944_create_floor_price_history_table.php

--- database/migrations/2023_12_11_202944_create_floor_price_history_table.php
+++ database/migrations/2023_12_11_202944_create_floor_price_history_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('floor_price_history', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Collection::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Collection::class)->constrained()->cascadeOnDelete()->index();
             $table->addColumn('numeric', 'floor_price', ['numeric_type' => 'numeric']);
             $table->foreignIdFor(Token::class, 'token_id')->nullable()->cascadeOnDelete();
             $table->timestamp('retrieved_at')->nullable()->index();
