173fb0f6f970ba7a4e4d4d6e961bf009d06e335d
Shahin Safaraliyev <shahin@ardenthq.com>
2023-12-14 17:16:13 +0400

    fix: use my collections URL for sorting

--- resources/js/Pages/Collections/MyCollections/Index.tsx
+++ resources/js/Pages/Collections/MyCollections/Index.tsx
@@ -24,7 +24,7 @@ const sort = ({
     selectedChainIds?: number[];
 }): void => {
     router.get(
-        route("collections"),
+        route("my.collections"),
         {
             ...getQueryParameters(),
             sort: sortBy,
