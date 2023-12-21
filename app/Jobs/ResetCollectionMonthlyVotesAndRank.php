e7c88b162d17efc36ba4e833bef62fbd1767ba68
Alfonso Bribiesca <alfonsobries@gmail.com>
2023-12-21 10:42:27 -0600

    update collection of the month votes n rank at start of the month

---
+++ app/Jobs/ResetCollectionMonthlyVotesAndRank.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use App\Models\Collection;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ResetCollectionMonthlyVotesAndRank implements ShouldBeUnique, ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Collection::updateMonthlyRankAndVotes();
+    }
+}
