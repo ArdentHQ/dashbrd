876937da664568574fe44796871bff85ec13e1b3
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2024-01-19 17:00:40 +0100

    wip

---
+++ app/Models/Traits/HasVolume.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Traits;
+
+use App\Enums\Period;
+use App\Models\TradingVolume;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+trait HasVolume
+{
+    /**
+     * @return HasMany<TradingVolume>
+     */
+    public function volumes(): HasMany
+    {
+        return $this->hasMany(TradingVolume::class);
+    }
+
+    /**
+     * Calculate the average collection volume since the given time period.
+     */
+    public function averageVolumeSince(Carbon $date): string
+    {
+        return $this->volumes()
+                    ->selectRaw('avg(volume::numeric) as aggregate')
+                    ->where('created_at', '>', $date)
+                    ->value('aggregate');
+    }
+
+    /**
+     * Get the collection volume, but respecting the volume in the given period.
+     * If no period is provided, get the total collection volume.
+     */
+    public function getVolume(?Period $period = null): ?string
+    {
+        return match ($period) {
+            Period::DAY => $this->avg_volume_1d,
+            Period::WEEK => $this->avg_volume_7d,
+            Period::MONTH => $this->avg_volume_30d,
+            default => $this->total_volume,
+        };
+    }
+
+    /**
+     * Scope the query to order the collections by the volume, but respecting the volume in the given period.
+     *
+     * @param  Builder<self>  $query
+     * @return Builder<self>
+     */
+    public function scopeOrderByVolume(Builder $query, ?Period $period = null): Builder
+    {
+        $column = match ($period) {
+            Period::DAY => 'avg_volume_1d',
+            Period::WEEK => 'avg_volume_7d',
+            Period::MONTH => 'avg_volume_30d',
+            default => 'total_volume',
+        };
+
+        return $query->orderByWithNulls($column.'::numeric', 'desc');
+    }
+}
