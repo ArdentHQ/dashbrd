16aebbf4d4eaa6fcbf743a40f4533af849ebf140
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2024-01-19 15:00:29 +0100

    Update NominatableCollectionControllerTest.php

--- tests/App/Http/Controllers/Api/NominatableCollectionControllerTest.php
+++ tests/App/Http/Controllers/Api/NominatableCollectionControllerTest.php
@@ -3,12 +3,18 @@
 declare(strict_types=1);
 
 use App\Models\Collection;
+use App\Models\Network;
+use App\Models\Token;
 use App\Models\User;
 
 it('can get all collections that can be nominated', function () {
+    Token::factory()->matic()->create();
+
+    $network = Network::polygon();
+
     $user = User::factory()->create();
 
-    Collection::factory(3)->create();
+    Collection::factory(3)->for($network)->create();
 
     $this->actingAs($user)
         ->get(route('nominatable-collections'))
