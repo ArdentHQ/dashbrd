b6a2642792e290d5ac5899d34282176166d8f6cc
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2024-01-22 11:28:22 +0100

    wip

--- database/migrations/2024_01_22_102544_add_volume_sum_columns_to_collections_table.php
+++ database/migrations/2024_01_22_102544_add_volume_sum_columns_to_collections_table.php
@@ -14,9 +14,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('collections', function (Blueprint $table) {
-            $table->string('avg_volume_1d')->nullable();
-            $table->string('avg_volume_7d')->nullable();
-            $table->string('avg_volume_30d')->nullable();
+            $table->string('volume_1d')->nullable();
+            $table->string('volume_7d')->nullable();
+            $table->string('volume_30d')->nullable();
+
+            $table->dropColumn([
+                'avg_volume_1d',
+                'avg_volume_7d',
+                'avg_volume_30d',
+            ]);
         });
     }
 };
