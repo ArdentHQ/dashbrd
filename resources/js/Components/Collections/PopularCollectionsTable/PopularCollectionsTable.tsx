8779e7d6d0972ecbc2b6db2ab2ecd097a906503f
crnkovic <crnkovic@users.noreply.github.com>
2024-01-31 11:36:51 +0000

    style: resolve linting violations

--- resources/js/Components/Collections/PopularCollectionsTable/PopularCollectionsTable.tsx
+++ resources/js/Components/Collections/PopularCollectionsTable/PopularCollectionsTable.tsx
@@ -59,7 +59,7 @@ export const PopularCollectionsTable = ({
                 data-testid="PopularCollectionsTable__SkeletonTable"
                 headerClassName="hidden md-lg:table-header-group"
                 variant="list"
-                columns={columns as Column<{ index: number }>[]}
+                columns={columns as Array<Column<{ index: number }>>}
                 data={Array.from({ length: 6 }, (x, index) => ({ index }))}
                 row={({ index }) => (
                     <PopularCollectionsTableItemSkeleton
@@ -80,7 +80,7 @@ export const PopularCollectionsTable = ({
             data-testid="PopularCollectionsTable"
             headerClassName="hidden md-lg:table-header-group"
             variant="list"
-            columns={columns as Column<App.Data.Collections.PopularCollectionData>[]}
+            columns={columns as Array<Column<App.Data.Collections.PopularCollectionData>>}
             manualSortBy={true}
             data={collections}
             row={(collection: App.Data.Collections.PopularCollectionData) => (
