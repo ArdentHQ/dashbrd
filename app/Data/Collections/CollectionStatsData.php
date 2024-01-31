0eb891ea3a37283e657e7043a5041aaf24737bf3
Josip Crnković <crnkovic@users.noreply.github.com>
2024-01-31 09:23:30 +0100

    refactor: cache "collections of the month" on the homepage (#640)

--- app/Data/Collections/CollectionStatsData.php
+++ app/Data/Collections/CollectionStatsData.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace App\Data\Collections;
 
-use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Data;
-use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 
 #[TypeScript]
-#[MapInputName(SnakeCaseMapper::class)]
 class CollectionStatsData extends Data
 {
     public function __construct(
--- app/Http/Controllers/PopularCollectionController.php
+++ app/Http/Controllers/PopularCollectionController.php
@@ -12,8 +12,6 @@ use App\Enums\Period;
 use App\Http\Controllers\Traits\HasCollectionFilters;
 use App\Models\Collection;
 use App\Models\Nft;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Inertia\Inertia;
@@ -23,7 +21,7 @@ class PopularCollectionController extends Controller
 {
     use HasCollectionFilters;
 
-    public function index(Request $request): Response|JsonResponse|RedirectResponse
+    public function index(Request $request): Response
     {
         $user = $request->user();
 
@@ -50,31 +48,29 @@ class PopularCollectionController extends Controller
         };
 
         $collections = Collection::query()
-            ->searchByName($request->get('query'))
-            ->when($sortBy === null, fn ($q) => $q->orderByVolume($period, currency: $chainId === null ? $currency : null))
-            ->when($sortBy === 'name', fn ($q) => $q->orderByName($sortDirection))
-            ->when($sortBy === 'value', fn ($q) => $q->orderByValue(null, $sortDirection, $currency))
-            ->when($sortBy === 'floor-price', fn ($q) => $q->orderByFloorPrice($sortDirection, $currency))
-            ->when($sortBy === 'chain', fn ($q) => $q->orderByChainId($sortDirection))
-            ->filterByChainId($chainId)
-            ->orderByFloorPrice('desc', $currency)
-            ->with([
-                'network',
-                'network.nativeToken',
-                'floorPriceToken',
-                'nfts' => fn ($q) => $q->limit(4),
-            ])
-            ->withCount('nfts')
-            ->paginate($perPage)
-            ->withQueryString();
+                            ->searchByName($request->get('query'))
+                            ->when($sortBy === null, fn ($q) => $q->orderByVolume($period, currency: $chainId === null ? $currency : null))
+                            ->when($sortBy === 'name', fn ($q) => $q->orderByName($sortDirection))
+                            ->when($sortBy === 'value', fn ($q) => $q->orderByValue(null, $sortDirection, $currency))
+                            ->when($sortBy === 'floor-price', fn ($q) => $q->orderByFloorPrice($sortDirection, $currency))
+                            ->when($sortBy === 'chain', fn ($q) => $q->orderByChainId($sortDirection))
+                            ->filterByChainId($chainId)
+                            ->orderByFloorPrice('desc', $currency)
+                            ->with([
+                                'network',
+                                'network.nativeToken',
+                                'floorPriceToken',
+                                'nfts' => fn ($q) => $q->limit(4),
+                            ])
+                            ->withCount('nfts')
+                            ->paginate($perPage)
+                            ->withQueryString();
 
-        $stats = Cache::remember('popular-collections-stats', now()->addHour(), function () {
-            return [
-                'fiatValues' => collect(Collection::getFiatValueSum()),
-                'collectionsCount' => Collection::query()->count(),
-                'nftsCount' => Nft::query()->count(),
-            ];
-        });
+        $stats = Cache::remember('popular-collections-stats', now()->addHour(), fn () => [
+            'fiatValues' => collect(Collection::getFiatValueSum()),
+            'collectionsCount' => Collection::count(),
+            'nftsCount' => Nft::count(),
+        ]);
 
         return Inertia::render('Collections/CollectionsCatalog/Index', [
             'title' => trans('metatags.popular-collections.title'),
