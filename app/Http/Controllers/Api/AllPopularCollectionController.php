0f254f45174a014bd6a59a6b5e7aca8a9e1bcae0
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2024-02-01 19:02:02 +0100

    Create AllPopularCollectionController.php

---
+++ app/Http/Controllers/Api/AllPopularCollectionController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Data\Collections\CollectionData;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Traits\HasCollectionFilters;
+use App\Http\Requests\FilterableCollectionRequest;
+use App\Repositories\CollectionRepository;
+use Illuminate\Http\JsonResponse;
+
+class AllPopularCollectionController extends Controller
+{
+    use HasCollectionFilters;
+
+    /**
+     * Get all of the popular collections.
+     */
+    public function index(FilterableCollectionRequest $request, CollectionRepository $collections): JsonResponse
+    {
+        $currency = $request->currency();
+        $period = $request->period();
+
+        /** @var string|null */
+        $sortBy = in_array($request->query('sort'), $this->allowedSortByValues) ? $request->query('sort') : null;
+
+        $collections = $collections->allPopular(
+            searchQuery: $request->get('query'),
+            period: $period,
+            chain: $request->chain(),
+            currency: $currency,
+            sortBy: $sortBy,
+            order: $request->sortDirection(default: $sortBy === null ? 'desc' : 'asc'),
+            perPage: $request->perPage(),
+        );
+
+        return response()->json([
+            'collections' => $collections->through(
+                fn ($collection) => CollectionData::fromModel($collection, $currency, volumePeriod: $period)
+            ),
+        ]);
+    }
+}
