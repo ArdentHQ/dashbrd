93e9875afc751dbad24fa2926b8d9dc29f913110
Josip Crnković <6536260+crnkovic@users.noreply.github.com>
2024-03-04 12:41:09 +0100

    add verifier middleware

---
+++ app/Http/Middleware/ValidAlchemyWebhook.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use RuntimeException;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidAlchemyWebhook
+{
+    /**
+     * Validate that the request is a verified Alchemy webhook request.
+     *
+     * @see https://docs.alchemy.com/reference/notify-api-quickstart#webhook-signature--security
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $token = config('services.alchemy.webhook_signature');
+
+        $hash = hash_hmac('sha256', $request->getContent(), $token);
+
+        if (! hash_equals($request->header('x-alchemy-signature'), $hash)) {
+            throw new RuntimeException('Not a valid Alchemy request.');
+        }
+
+        return $next($request);
+    }
+}
